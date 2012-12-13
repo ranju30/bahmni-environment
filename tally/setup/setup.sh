5eeab6ea9c37ff87f3e33415c7522cc45d8e891f
pchandra <pchandra@thoughtworks.com>
2012-12-13 11:23:20 +0530

    pravee|adding initial set of scripts to setup dummy data for tally

---
+++ tally/setup/setup.sh
@@ -0,0 +1,4 @@
+curl -X POST -d @new_unit.xml  http://vishal:9000
+curl -X POST -d @new_stock_group.xml  http://vishal:9000
+curl -X POST -d @new_stock_item.xml  http://vishal:9000
+curl -X POST -d @new_patient_ledger.xml  http://vishal:9000
