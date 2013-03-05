f75104a92bb0d536551a4c3dc79c2ebd133b13ad
sushmitharaos <sushmitharaos@gmail.com>
2013-03-05 16:49:53 +0530

    Sush|making the shell script more verbose

--- puppet/modules/openerp/templates/install_openerp.sh
+++ puppet/modules/openerp/templates/install_openerp.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 # Modified script from Carlos E. Fonseca Zorrilla
-yum -vy install wget unzip
+yum -v -y install wget unzip
 rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 rpm -ivh http://yum.pgrpms.org/9.2/redhat/rhel-6-i386/pgdg-centos92-9.2-6.noarch.rpm
 
-yum -vy install python-psycopg2 python-lxml PyXML python-setuptools libxslt-python pytz
-yum -vy install python-matplotlib python-babel python-mako python-dateutil python-psycopg2
-yum -vy install pychart pydot python-reportlab python-devel python-imaging python-vobject
-yum -vy install hippo-canvas-python mx python-gdata python-ldap python-openid
-yum -vy install python-werkzeug python-vatnumber pygtk2 glade3 pydot python-dateutil
-yum -vy install python-matplotlib pygtk2 glade3 pydot python-dateutil python-matplotlib
-yum -vy install python python-devel python-psutil python-docutils make
-yum -vy install automake gcc gcc-c++ kernel-devel byacc flashplugin-nonfree poppler-utils pywebdav
-yum -vy install postgresql92-libs postgresql92-server postgresql92
+echo "##################################################"
+echo "installing all python dependencies for openerp"
+echo "##################################################"
+
+yum -v -y install python-psycopg2 python-lxml PyXML python-setuptools libxslt-python pytz
+yum -v -y install python-matplotlib python-babel python-mako python-dateutil python-psycopg2
+yum -v -y install pychart pydot python-reportlab python-devel python-imaging python-vobject
+yum -v -y install hippo-canvas-python mx python-gdata python-ldap python-openid
+yum -v -y install python-werkzeug python-vatnumber pygtk2 glade3 pydot python-dateutil
+yum -v -y install python-matplotlib pygtk2 glade3 pydot python-dateutil python-matplotlib
+yum -v -y install python python-devel python-psutil python-docutils make
+yum -v -y install automake gcc gcc-c++ kernel-devel byacc flashplugin-nonfree poppler-utils pywebdav
 
 easy_install http://cheeseshop.python.org/packages/source/p/pyparsing/pyparsing-1.5.5.tar.gz
 
+echo "##################################################"
+echo "Installing postgres"
+echo "##################################################"
+yum -v -y install postgresql92-libs postgresql92-server postgresql92
 service postgresql-9.2 initdb
 chkconfig postgresql-9.2 on
 service postgresql-9.2 start
@@ -37,6 +44,10 @@ if [ ! -d $NAME ]; then
 fi
 done
 rm -rf openerp*
+
+echo "##################################################"
+echo "Installing OpenERP"
+echo "##################################################"
 wget http://nightly.openerp.com/7.0/nightly/src/openerp-7.0-latest.tar.gz
 tar -zxvf openerp-7.0-latest.tar.gz  --transform 's!^[^/]\+\($\|/\)!openerp\1!'
 cd openerp
