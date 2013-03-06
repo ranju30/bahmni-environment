6ff54f51e72b029d8ca6c14348ec3604e31184bf
pchandra <pchandra@thoughtworks.com>
2013-03-06 12:14:19 +0530

    praveen|cleaning up deploy script

--- deploy/openerp/scripts/deploy.sh
+++ deploy/openerp/scripts/deploy.sh
@@ -1,4 +1,4 @@
-rm -rf /tmp/openerp-modules
+rm -rf /tmp/openerp/
 git clone https://github.com/Bhamni/openerp-modules.git /tmp
 OPENERP_ROOT=`cat $1.properties | grep openerp_deploy_root | cut -f2 -d"="`
-cp -r /tmp/openerp-modules/* $OPENERP_ROOT
+cp -r /tmp/openerp/* $OPENERP_ROOT
