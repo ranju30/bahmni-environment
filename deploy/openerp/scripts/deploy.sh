d9b95366e44e09d13a2a697a9c41aad06934c043
pchandra <pchandra@thoughtworks.com>
2013-03-06 10:54:09 +0530

    praveen|deploy scripts for openerp

---
+++ deploy/openerp/scripts/deploy.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/Bhamni/openerp-modules.git /tmp
+
+OPENERP_ROOT=`cat $1.properties | grep openerp_deploy_root | cut -f2 -d"="`
+cp -r /tmp/openerp-modules/* $OPENERP_ROOT
