a68d820416b10c21594440ec5a9078f58c1cb91c
pchandra <pchandra@thoughtworks.com>
2013-03-06 12:04:09 +0530

    praveen|fetch mdoules from git

--- deploy/openerp/scripts/deploy.sh
+++ deploy/openerp/scripts/deploy.sh
@@ -1,4 +1,4 @@
+rm -rf /tmp/openerp-modules
 git clone https://github.com/Bhamni/openerp-modules.git /tmp
-
 OPENERP_ROOT=`cat $1.properties | grep openerp_deploy_root | cut -f2 -d"="`
 cp -r /tmp/openerp-modules/* $OPENERP_ROOT
