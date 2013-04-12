1c873501908d4bc88f5bc478855349b228f3b7fd
sushmitharaos <sushmitharaos@gmail.com>
2013-04-12 11:10:45 +0530

    RT,Sush|#824|copy web.xml with changed to ensure log4j.xml config reload

--- puppet/modules/openmrs/manifests/init.pp
+++ puppet/modules/openmrs/manifests/init.pp
@@ -1,31 +1,38 @@
 class openmrs ( $tomcatInstallationDirectory) {
     exec {"download-openmrs-war" :
-            command     => "/usr/bin/wget -O /tmp/openmrs-${openMRSVersion}.war http://sourceforge.net/projects/openmrs/files/releases/OpenMRS_${openMRSVersion}/openmrs.war",
-            timeout     => 0,
-            provider    => "shell",
-			      user        => "${jssUser}",
-            onlyif      => "! test -f /tmp/openmrs-${openMRSVersion}.war"
+        command     => "/usr/bin/wget -O /tmp/openmrs-${openMRSVersion}.war http://sourceforge.net/projects/openmrs/files/releases/OpenMRS_${openMRSVersion}/openmrs.war",
+        timeout     => 0,
+        provider    => "shell",
+        user        => "${jssUser}",
+        onlyif      => "! test -f /tmp/openmrs-${openMRSVersion}.war"
     }
-    
+
     exec {"install-openmrs" :
-            command     => "cp /tmp/openmrs-${openMRSVersion}.war ${tomcatInstallationDirectory}/webapps/openmrs.war",
-			      user        => "${jssUser}",
-            onlyif      => "test -d ${tomcatInstallationDirectory}",
-            require		=> Exec["download-openmrs-war"],            
+        command     => "cp /tmp/openmrs-${openMRSVersion}.war ${tomcatInstallationDirectory}/webapps/openmrs.war",
+        user        => "${jssUser}",
+        onlyif      => "test -d ${tomcatInstallationDirectory}",
+        require		=> Exec["download-openmrs-war"],
     }
-    
-    file { "/home/${jssUser}/.OpenMRS/openerp.properties" :
-        ensure      => present,
-        content     => template("openmrs/openerp.properties.erb"),
+
+    file { "/home/${jssUser}/.OpenMRS" :
+        ensure      => directory,
         owner       => "${jssUser}",
         group       => "${jssUser}",
         require    => Exec["install-openmrs"],
     }
 
+    file { "/home/${jssUser}/.OpenMRS/openerp.properties" :
+        ensure      => present,
+        content     => template("openmrs/openerp.properties.erb"),
+        owner       => "${jssUser}",
+        group       => "${jssUser}",
+        require    => File["/home/${jssUser}/.OpenMRS"],
+    }
+
     exec {"restart_tomcat" :
-          command     => "/etc/init.d/tomcat restart",
-          user        => "${jssUser}",
-          require		=> Exec["install-openmrs"],
+        command     => "/etc/init.d/tomcat restart",
+        user        => "${jssUser}",
+        require		=> Exec["install-openmrs"],
   	}
 
     file { "$tomcatInstallationDirectory/webapps/openmrs/WEB-INF/classes/log4j.xml" :
@@ -34,5 +41,13 @@ class openmrs ( $tomcatInstallationDirectory) {
         owner       => "${jssUser}",
         group       => "${jssUser}",
         require    => Exec["restart_tomcat"],
+    }
+
+    file { "$tomcatInstallationDirectory/webapps/openmrs/WEB-INF/web.xml" :
+        ensure      => present,
+        content     => template("openmrs/web.xml"),
+        owner       => "${jssUser}",
+        group       => "${jssUser}",
+        require    => File["$tomcatInstallationDirectory/webapps/openmrs/WEB-INF/classes/log4j.xml"],
     }
 }
