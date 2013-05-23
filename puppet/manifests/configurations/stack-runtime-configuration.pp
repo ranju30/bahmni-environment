dd0fb106af86890aaf8e975dddcda97c81981265
Vivek Singh <petmongrels@gmail.com>
2013-05-23 19:31:10 +0530

    added openmrs password property to flyway

--- puppet/manifests/configurations/stack-runtime-configuration.pp
+++ puppet/manifests/configurations/stack-runtime-configuration.pp
@@ -85,7 +85,7 @@ $nagios_user = "nagios"
 # OpenMRS
 $openmrs_database_user = "openmrs-user"
 $openmrs_database_password = "password"
-$openmrs_password = "Admin123"
+$openmrs_password = "test"
 $idgen_omod_file = "idgen-2.4.1.omod"
 
 # OpenERP properties used by OpenMRS
