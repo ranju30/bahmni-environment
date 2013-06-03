c1fa25403e5fadc198f5fec047c9910015892a60
Deepak N <endeep123@gmail.com>
2013-06-03 19:14:41 +0530

    D3 | Added --update option to rsync

--- puppet/modules/jss-cron/manifests/init.pp
+++ puppet/modules/jss-cron/manifests/init.pp
@@ -1,7 +1,7 @@
 class jss-cron {
     #--delete option is taken out from rsync intentionally. As slave folder acts as backup.
     cron { "sync_patient_image_cron" :
-        command => "rsync -avz --progress -i --itemize-changes -p ${imagesDirectory} -e ssh root@${secondary_machine_ip}:${imagesDirectory}/../",
+        command => "rsync -avz --progress --update -i --itemize-changes -p ${imagesDirectory} -e ssh root@${secondary_machine_ip}:${imagesDirectory}/../",
         user    => "root",
         minute  => "*/1"
     }
