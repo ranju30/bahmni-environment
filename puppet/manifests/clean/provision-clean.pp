34802a04814a8783eddc7efaf112ae45e265fef5
Vivek Singh <petmongrels@gmail.com>
2013-07-04 14:54:23 +0530

    Mujir, Vivek | Dropping openmrs db before restoring the dump Added ProxyPreserve to ensure the server gets the requested url

---
+++ puppet/manifests/clean/provision-clean.pp
@@ -0,0 +1,7 @@
+import "configurations/node-configuration"
+import "configurations/stack-installers-configuration"
+import "configurations/stack-runtime-configuration"
+import "configurations/deployment-configuration"
+
+node default {
+}
