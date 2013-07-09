c6fc8b534892bf7d669d3763a63895dafda8f2c0
Deepak N <endeep123@gmail.com>
2013-07-09 16:05:43 +0530

    Fixing artficatory client permissions

--- puppet/manifests/configurations/artifactory-configuration.pp
+++ puppet/manifests/configurations/artifactory-configuration.pp
@@ -1,3 +1,4 @@
 #client configuration
-$artifactory_server_address=172.18.2.1:8081 # ipaddress:port or domainname:port or proxyaddress
-$m2_folder="/var/go/.m2/" # Expand users ~/.m2
+$artifactory_server_address="172.18.2.1:8081" # ipaddress:port or domainname:port or proxyaddress
+$maven_user="go"
+$m2_folder="/var/go/.m2" # Expand users ~/.m2
--- puppet/modules/artifactory/manifests/client.pp
+++ puppet/modules/artifactory/manifests/client.pp
@@ -2,6 +2,8 @@ class artifactory::client {
     file { "${m2_folder}/settings.xml" :
       ensure      => present,
       content     => template("artifactory/client/settings.xml.erb"),
+      owner       => "${maven_user}",
+      group       => "${maven_user}",
       mode        => 644,
     }
 }
