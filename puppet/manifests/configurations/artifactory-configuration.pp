44b1430097e1ca123cf25d734f1dafd5a62e54ac
Deepak N <endeep123@gmail.com>
2013-07-11 17:32:45 +0530

    Using http instead of https for artifactory

--- puppet/manifests/configurations/artifactory-configuration.pp
+++ puppet/manifests/configurations/artifactory-configuration.pp
@@ -1,8 +1,8 @@
 #Apache as proxy server
 import "httpd-default-configuration"
-$httpsRedirects = [{path => "/artifactory", redirectPath => "http://localhost:8081/artifactory"}]
+$httpRedirects = [{path => "/artifactory", redirectPath => "http://localhost:8081/artifactory"}]
 
 #client configuration
-$artifactory_server_base_url="https://bahmnirepo.thoughtworks.com/artifactory"
+$artifactory_server_base_url="http://bahmnirepo.thoughtworks.com/artifactory"
 $maven_user="go"
 $m2_folder="/var/go/.m2" # Expand users ~/.m2
