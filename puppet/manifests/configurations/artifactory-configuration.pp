8104c253ab8b4fa88a5e0ca7bf3a78b0211f4dca
Deepak N <endeep123@gmail.com>
2013-07-11 15:44:12 +0530

    Using dns name instead of ip for bahmni repo

--- puppet/manifests/configurations/artifactory-configuration.pp
+++ puppet/manifests/configurations/artifactory-configuration.pp
@@ -3,6 +3,6 @@ import "httpd-default-configuration"
 $httpsRedirects = [{path => "/artifactory", redirectPath => "http://localhost:8081/artifactory"}]
 
 #client configuration
-$artifactory_server_address="172.18.2.1:8081" # ipaddress:port or domainname:port or proxyaddress
+$artifactory_server_base_url="https://bahmnirepo.thoughtworks.com/artifactory"
 $maven_user="go"
 $m2_folder="/var/go/.m2" # Expand users ~/.m2
