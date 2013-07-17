2f23bd19ffffe15d67fd86dd6ef5e414d48f2669
Deepak N <endeep123@gmail.com>
2013-07-17 14:19:22 +0530

    Fixing config file

--- puppet/manifests/configurations/stack-installers-configuration.pp
+++ puppet/manifests/configurations/stack-installers-configuration.pp
@@ -6,4 +6,4 @@ $tomcat_version="7.0.22"
 $openerp_installer_file="openerp-7.0-20130301-002301.tar.gz"
 $mysql_connector_java_version="5.1.18"
 $openerp_install_location="/usr/lib/python2.6/site-packages/openerp-7.0_20130301_002301-py2.6.egg"
-$openmrs_war_file_name=bahmni-openmrs-webapp-1.9.4-SNAPSHOT.war
+$openmrs_war_file_name="bahmni-openmrs-webapp-1.9.4-SNAPSHOT.war"
