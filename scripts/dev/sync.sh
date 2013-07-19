21a5c2f26c651d16507a233e8389f376971d4c14
Sneha Satish <yssneha@gmail.com>
2013-07-19 17:13:40 +0530

    d3/sneha| script to watch files and sync

---
+++ scripts/dev/sync.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ $# -lt 3 ]; then
+   echo "Usage: $0 <local_folder> <remote_ip> <remote_folder>"
+   echo "Eg: scripts/dev/sync.sh ../registration/app/ 10.4.33.25 /var/www/registration"
+   exit 1
+fi
+
+local=$1
+remote=$2
+path=$3
+sync_command="date +%H:%M:%S && rsync -iru --exclude .git  --progress --itemize-changes --update --delete --chmod=Du=r,Dg=rwx,Do=rwx,Fu=rwx,Fg=rwx,Fo=rwx -p $local -e ssh root@$remote:$path"
+sh -c "$sync_command"
+fswatch $local "$sync_command"
