8feaa1555b34ff37e33fbe3c54078bed0433265f
Vinay Venu <vinayv@thoughtworks.com>
2013-11-07 11:00:52 +0530

    Vinay | Make puppet return proper exit codes.

--- scripts/run-module.sh
+++ scripts/run-module.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-set -e -x 
-
 SCRIPTS_DIR=`dirname $0`
 BASE_DIR=$SCRIPTS_DIR/..
 FACTER_module_name="$1" puppet apply $BASE_DIR/puppet/manifests/run.pp  --modulepath=$BASE_DIR/puppet/modules/
+RETURN_CODE=$?
+if [ $RETURN_CODE -ne 2 ]
+then
+	echo "Error running script. Return code = ${RETURN_CODE}. Exiting"
+	exit 1
+fi
--- scripts/run-puppet-manifest.sh
+++ scripts/run-puppet-manifest.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-set -e -x 
-
 SCRIPTS_DIR=`dirname $0`
 BASE_DIR=$SCRIPTS_DIR/..
-puppet apply $BASE_DIR/puppet/manifests/$1.pp --modulepath=$BASE_DIR/puppet/modules/
+puppet apply $BASE_DIR/puppet/manifests/$1.pp --modulepath=$BASE_DIR/puppet/modules/ --detailed-exitcodes
+RETURN_CODE=$?
+if [ $RETURN_CODE -ne 2 ]
+then
+	echo "Error running script. Return code = ${RETURN_CODE}. Exiting"
+	exit 1
+fi
