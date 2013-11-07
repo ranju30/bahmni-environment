32375cd2d1b4be51bca25275ba49def85c54e377
Vinay Venu <vinayv@thoughtworks.com>
2013-11-07 11:26:56 +0530

    Vinay | Make puppet run scripts a bit better Add zero to the list of good exit codes. This means no change done by puppet. Add usage information Add guard clause for missing module/package

--- scripts/run-module.sh
+++ scripts/run-module.sh
@@ -4,9 +4,23 @@ set -e
 
 SCRIPTS_DIR=`dirname $0`
 BASE_DIR=$SCRIPTS_DIR/..
-FACTER_module_to_run=$1 puppet apply $BASE_DIR/puppet/manifests/run.pp  --modulepath=$BASE_DIR/puppet/modules/  --detailed-exitcodes
+
+usage() {
+	echo "Usage: run-module.sh <module-name>"
+	echo "<module-name> is the name of a puppet module present in ${BASE_DIR}/puppet/modules directory"
+}
+
+MODULE_NAME=$1
+if [ "${MODULE_NAME}a" = "a" ]
+then
+	echo "Please specify a module to run)"
+	usage
+	exit 1
+fi
+
+FACTER_module_to_run=$MODULE_NAME puppet apply $BASE_DIR/puppet/manifests/run.pp  --modulepath=$BASE_DIR/puppet/modules/  --detailed-exitcodes
 RETURN_CODE=$?
-if [ $RETURN_CODE -ne 2 ]
+if [ $RETURN_CODE -ne 0 ] && [ $RETURN_CODE -ne 2 ]
 then
 	echo "Error running script. Return code = ${RETURN_CODE}. Exiting"
 	exit 1
--- scripts/run-puppet-manifest.sh
+++ scripts/run-puppet-manifest.sh
@@ -2,9 +2,23 @@
 
 SCRIPTS_DIR=`dirname $0`
 BASE_DIR=$SCRIPTS_DIR/..
+
+usage() {
+	echo "Usage: run-puppet-manifest.sh <manifest-file>"
+	echo "<manifest-file> is the name of a manifest file present in ${BASE_DIR}/puppet/manifests directory"
+}
+
+MODULE_NAME=$1
+if [ "${MODULE_NAME}a" = "a" ]
+then
+	echo "Please specify a module to run)"
+	usage
+	exit 1
+fi
+
 puppet apply $BASE_DIR/puppet/manifests/$1.pp --modulepath=$BASE_DIR/puppet/modules/ --detailed-exitcodes
 RETURN_CODE=$?
-if [ $RETURN_CODE -ne 2 ]
+if [ $RETURN_CODE -ne 0 ] && [ $RETURN_CODE -ne 2 ]
 then
 	echo "Error running script. Return code = ${RETURN_CODE}. Exiting"
 	exit 1
