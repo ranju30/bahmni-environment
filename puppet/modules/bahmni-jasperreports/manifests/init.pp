416bc21c1dd6a46736e674a30d8d803ccefc3bf0
Vinay Venu <vinayv@thoughtworks.com>
2013-11-08 17:12:46 +0530

    Vinay | Add Jasper deploy script

---
+++ puppet/modules/bahmni-jasperreports/manifests/init.pp
@@ -0,0 +1,18 @@
+class bahmni-jasperreports {
+
+	$properties_file = 'reports_default.properties'
+	
+	exec { "bahmni-jasperserver-deploy-reports" :
+    provider => "shell",	
+		command => "scripts/deploy.sh -j $jasperHome -p ../conf/${properties_file} ${deployment_log_expression}",
+		path    => "${os_path}",
+    	cwd     => "${build_output_dir}/jss-reports",
+	}
+
+	exec { "bahmni-jasperserver-deploy-customserver" :
+    provider => "shell",	
+		command => "scripts/deployCustomJasperServer.sh $jasperHome ${deployment_log_expression}",
+		path    => "${os_path}",
+    	cwd     => "${build_output_dir}/jss-reports",
+	}
+}
