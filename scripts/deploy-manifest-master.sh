fb685132735e6a9571d48753dd4ad1b2fc170e4b
Vinay Venu <vinayv@thoughtworks.com>
2013-11-15 13:43:06 +0530

    Vinay | #0 | These scripts have been removed from CI and are not required anymore.

--- scripts/deploy-manifest-master.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-export FACTER_openmrs_distro_file_name_prefix=distro-2.5-SNAPSHOT
-export FACTER_openmrs_war_file_name="openmrs-webapp-1.9.7-SNAPSHOT"
-
-`dirname $0`/deploy-puppet-manifest.sh $@
