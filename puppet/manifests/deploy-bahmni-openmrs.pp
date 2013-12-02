abd68ce0cf86dafcdb9a9af27d062a326cc17b65
Deepak N <endeep123@gmail.com>
2013-12-02 11:30:36 +0530

    Fixing bahmni openmrs deploy

--- puppet/manifests/deploy-bahmni-openmrs.pp
+++ puppet/manifests/deploy-bahmni-openmrs.pp
@@ -2,6 +2,7 @@ import "configurations/node-configuration"
 import "configurations/stack-installers-configuration"
 import "configurations/stack-runtime-configuration"
 import "configurations/deployment-configuration"
+import "configurations/openmrs-versions-configuration.pp"
 
 # pre-condition
 # bahmni-stop must have been run before this
