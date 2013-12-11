f52785529bb5fd819a6e08a40ecc040fd73583da
mujir <mujir@hotmail.com>
2013-12-11 12:04:44 +0530

    Mujir | fixed issue with openerp modules not being updated on deploy. 1. taking into account we have openerp-modules zip file, we need to copy from within openerp-modules to openerp folder

--- puppet/modules/bahmni-openerp/manifests/init.pp
+++ puppet/modules/bahmni-openerp/manifests/init.pp
@@ -1,5 +1,6 @@
 class bahmni-openerp {
     $log4j_xml_file = "${tomcatInstallationDirectory}/webapps/${openerp_atomfeed_war_file_name}/WEB-INF/classes/log4j.xml"
+    $openerp_modules_zip_filename = "openerp-modules"
 
 
 	file { "${bahmni_openerp_temp_dir}" :
@@ -9,7 +10,7 @@ class bahmni-openerp {
 
 	exec { "bahmni_openerp_codebase" :
     provider => "shell",	
-		command => "unzip -o -q ${build_output_dir}/openerp-modules.zip -d ${bahmni_openerp_temp_dir} ${deployment_log_expression}",
+		command => "unzip -o -q ${build_output_dir}/${openerp_modules_zip_filename}.zip -d ${bahmni_openerp_temp_dir} ${deployment_log_expression}",
 		path => "${os_path}",
 		require => File["${bahmni_openerp_temp_dir}"]
 	}
@@ -31,7 +32,7 @@ class bahmni-openerp {
 	
 	exec { "bahmni_openerp" :
 	  	provider => "shell",
-		command => "cp -r ${bahmni_openerp_temp_dir}/* ${openerp_install_location}/openerp/addons ${deployment_log_expression}",
+		command => "cp -r ${bahmni_openerp_temp_dir}/${openerp_modules_zip_filename}/* ${openerp_install_location}/openerp/addons ${deployment_log_expression}",
 		path => "${os_path}",
 		user => "${openerpShellUser}",
 		group => "${openerpGroup}",
