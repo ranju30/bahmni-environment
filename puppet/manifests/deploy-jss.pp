a53e66ac7c3dd4024b7ee0827f2c69803754a059
Vinay Venu <vinayv@thoughtworks.com>
2014-02-10 16:57:00 +0530

    Hack script so it actually works

--- puppet/manifests/deploy-jss.pp
+++ puppet/manifests/deploy-jss.pp
@@ -1,8 +1,20 @@
-import "deploy-jss-bahmni-openmrs.pp"
+import "configurations/node-configuration"
+import "configurations/stack-installers-configuration"
+import "configurations/stack-runtime-configuration"
+import "configurations/deployment-configuration"
+import "configurations/openmrs-versions-configuration.pp"
 
-node deploy-jss {
+node default {
+  include bahmni-configuration
   include httpd
-  include deploy-jss-bahmni-openmrs
+  class { 'bahmni-data' :
+    require => Class["openmrs"],
+  }
+  include bahmni-webapps
+  include bahmni-ui-apps
+  class { 'implementation-config':
+    implementationName => "jss", require => Class['bahmni-webapps'],
+  }
   include bahmni-openerp
   include openelis
   include reference-data
