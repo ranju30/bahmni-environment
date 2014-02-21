d23b8bd1df13b62e2a358bdafbd054b8587f422c
Gurpreet <gluthra@thoughtworks.com>
2014-02-21 16:24:30 +0530

    Gurpreet,Banka| Deleted file download-packages.pp. Not needed. Added this step into provision.

--- puppet/manifests/download-packages.pp
+++
@@ -1,9 +0,0 @@
-import "configurations/node-configuration"
-import "configurations/stack-installers-configuration"
-import "configurations/stack-runtime-configuration"
-import "configurations/deployment-configuration"
-import "configurations/openmrs-versions-configuration"
-
-node default {
-  include python-packages
-}
