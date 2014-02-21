a2c0a78a74c670dd7274245997d2c898067518d2
Gurpreet <gluthra@thoughtworks.com>
2014-02-21 14:23:14 +0530

    Gurpreet, Banka | Created package downloader puppet file. Making sure build_dir is created before downloading builds

---
+++ puppet/manifests/download-packages.pp
@@ -0,0 +1,9 @@
+import "configurations/node-configuration"
+import "configurations/stack-installers-configuration"
+import "configurations/stack-runtime-configuration"
+import "configurations/deployment-configuration"
+import "configurations/openmrs-versions-configuration"
+
+node default {
+  include python-packages
+}
