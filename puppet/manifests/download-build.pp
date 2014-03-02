49cd6a08e9bb4cf3256c4070ed7f041d0d9b4ccf
Gurpreet <gluthra@thoughtworks.com>
2014-03-02 17:59:36 +0530

    Gurpreet| Added support for GO server credentials for download of artifacts. Introduced two variables to hold creds in stack-runtime-configuration. Enhaned wget to use appropriate flags for creds based download

--- puppet/manifests/download-build.pp
+++ puppet/manifests/download-build.pp
@@ -1,6 +1,7 @@
 import "configurations/node-configuration"
 import "configurations/openmrs-versions-configuration.pp"
 import "configurations/stack-installers-configuration.pp"
+import "configurations/stack-runtime-configuration.pp"
 
 node default {
   include bahmni-artifacts
