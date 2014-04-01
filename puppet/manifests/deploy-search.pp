50919fa7cf1fd0ea1791ec07d1a5ee99894c8af7
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-04-01 22:03:18 +0530

    Gurpreet| Created a puppet deployment script for SEARCH. Very similar to deploy-jss.pp

---
+++ puppet/manifests/deploy-search.pp
@@ -0,0 +1,18 @@
+import "configurations/node-configuration"
+import "configurations/stack-installers-configuration"
+import "configurations/stack-runtime-configuration"
+import "configurations/deployment-configuration"
+import "configurations/openmrs-versions-configuration.pp"
+
+node default {
+  include bahmni-configuration
+  include httpd
+  include bahmni-webapps
+  include bahmni-ui-apps
+  class { 'implementation-config':
+    implementationName => "search", require => Class['bahmni-webapps'],
+  }
+  include bahmni-openerp
+  include openelis
+  include reference-data
+}
