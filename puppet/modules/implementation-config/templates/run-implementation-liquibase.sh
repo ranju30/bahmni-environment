8d5a45b0fd3fed651aa1daf0b1cf7b8a56642eb6
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-04-03 21:48:45 +0530

    Gurpreet| Changed all variable access to use @ prefix, since non-ampersand prefix has been deprecated in puppet. Was throwing warnings.

--- puppet/modules/implementation-config/templates/run-implementation-liquibase.sh
+++ puppet/modules/implementation-config/templates/run-implementation-liquibase.sh
@@ -4,11 +4,11 @@ set -e -x
  
 
 CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock"
-LIQUIBASE_JAR="<%= tomcatInstallationDirectory %>/webapps/openmrs/WEB-INF/lib/liquibase-core-2.0.5.jar"
+LIQUIBASE_JAR="<%= @tomcatInstallationDirectory %>/webapps/openmrs/WEB-INF/lib/liquibase-core-2.0.5.jar"
 DRIVER="com.mysql.jdbc.Driver"
 CREDS="--url=jdbc:mysql://localhost:3306/openmrs --username=root --password=password"
-CLASSPATH="<%= build_output_dir %>/<%= openmrs_distro_file_name_prefix %>/<%= openmrs_war_file_name %>.war"
-WORKING_DIR="<%= build_output_dir %>/<%= migrationsDirectory %>"
+CLASSPATH="<%= @build_output_dir %>/<%= @openmrs_distro_file_name_prefix %>/<%= @openmrs_war_file_name %>.war"
+WORKING_DIR="<%= @build_output_dir %>/<%= @migrationsDirectory %>"
 
 cd $WORKING_DIR 
 java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --classpath=$CLASSPATH --changeLogFile=liquibase.xml $CREDS update
--- puppet/modules/openmrs/templates/run-liquibase.sh
+++ puppet/modules/openmrs/templates/run-liquibase.sh
@@ -7,8 +7,8 @@ CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dli
 LIQUIBASE_JAR="openmrs/WEB-INF/lib/liquibase-core-2.0.5.jar"
 DRIVER="com.mysql.jdbc.Driver"
 CREDS="--url=jdbc:mysql://localhost:3306/openmrs --username=root --password=password"
-COMMON_CLASSPATH="<%= build_output_dir %>/<%= openmrs_distro_file_name_prefix %>/<%= openmrs_war_file_name %>.war"
-WORKING_DIR="<%= tomcatInstallationDirectory %>/webapps"
+COMMON_CLASSPATH="<%= @build_output_dir %>/<%= @openmrs_distro_file_name_prefix %>/<%= @openmrs_war_file_name %>.war"
+WORKING_DIR="<%= @tomcatInstallationDirectory %>/webapps"
 
 cd $WORKING_DIR
 java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --classpath=$COMMON_CLASSPATH --changeLogFile=liquibase-schema-only.xml $CREDS update
