b3a7711e4c66a873221811ea9b0736d5a6028b2b
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-04-06 10:34:09 +0530

    Gurpreet| #2075| Added deployment puppet file specific to search

---
+++ puppet/manifests/deploy-search-bahmni-openmrs.pp
@@ -0,0 +1,17 @@
+import "configurations/node-configuration"
+import "configurations/stack-installers-configuration"
+import "configurations/stack-runtime-configuration"
+import "configurations/deployment-configuration"
+import "configurations/openmrs-versions-configuration.pp"
+
+# pre-condition
+# bahmni-stop must have been run before this
+node default {
+  include bahmni-configuration
+  include httpd
+  include bahmni-webapps
+  include bahmni-ui-apps
+  class { 'implementation-config':
+    implementationName => "jss", require => Class['bahmni-webapps'],
+  }
+}
