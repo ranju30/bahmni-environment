782900ac52dee5ee4e7c99d02dbbd4440ccd5b0e
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-04-07 12:50:17 +0530

    Gurpreet| Undoing the files which were checked in by mistake.

--- puppet/manifests/test.pp
+++ puppet/manifests/test.pp
@@ -8,10 +8,5 @@ import "configurations/openmrs-versions-configuration"
 # bahmni-stop must have been run before this
 node default {
 #	include bahmni-artifacts
-	# include httpd
-
-	file { "/tmp/facts.yaml":
-    	content => inline_template("<%= scope.to_hash.reject { |k,v| !( k.is_a?(String) && v.is_a?(String) ) }.to_yaml %>"),
-    	ensure => present
-	}
+	include httpd
 }
