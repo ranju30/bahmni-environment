9e5fa40196a0baf05194e6ab34249e5be77cfe42
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-05-01 18:04:47 +0530

    Removed provision clean since it was doing nothing, and had incorrect configuration.

--- puppet/manifests/clean/provision-clean.pp
+++
@@ -1,7 +0,0 @@
-import "configurations/node-configuration"
-import "configurations/stack-installers-configuration"
-import "configurations/stack-runtime-configuration"
-import "configurations/deployment-configuration"
-
-node default {
-}
