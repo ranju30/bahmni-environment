11f4f54a60bd79103f2201d0636a9459dc434d6d
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-05-01 18:05:37 +0530

    Gurpreet: Added escape for backslash to remove puppet warning

--- puppet/modules/identifier-cron/manifests/init.pp
+++ puppet/modules/identifier-cron/manifests/init.pp
@@ -1,7 +1,7 @@
 class identifier-cron {
 
     cron { "reset_identifier_sequence_yearly" :
-        command 	=> "/usr/bin/mysql -u${openmrs_database_user} -p${openmrs_database_password} openmrs -e \"update idgen_seq_id_gen set next_sequence_value=1,suffix=concat('/',(SELECT DATE_FORMAT(now(),'\%y')));\"",
+        command 	=> "/usr/bin/mysql -u${openmrs_database_user} -p${openmrs_database_password} openmrs -e \"update idgen_seq_id_gen set next_sequence_value=1,suffix=concat('/',(SELECT DATE_FORMAT(now(),'\\%y')));\"",
         user      	=> "${bahmni_user}",
         hour        => 0,
         minute      => 0,
