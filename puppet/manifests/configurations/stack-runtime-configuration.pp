905a80984610081403c2467d00d72f06a97f0445
Deepak N <endeep123@gmail.com>
2014-05-22 10:10:33 +0530

    #2139 | add caching concepts

--- puppet/manifests/configurations/stack-runtime-configuration.pp
+++ puppet/manifests/configurations/stack-runtime-configuration.pp
@@ -118,8 +118,8 @@ $httpsAggressiveCacheDisabledDirectories = []
 $httpsSubdomains = [{subdomain => "openerp", url => "http://localhost:8069"}]
 
 $httpdCacheDirectory = "/var/cache/mod_proxy"
-# Example entry: {path => "/openmrs/ws/rest/v1/concept", expireTime => "20 minutes"}
-$httpsCacheUrls = [{path => "var/www/patient_images", expireTime => "86400"}]
+$httpsCacheUrls = [{path => "var/www/patient_images", type => 'Directory', expireTime => "86400"},
+ 				   {path => "/openmrs/ws/rest/v1/concept"}]
 ######################## HTTPD CONFIG END################################################
 
 # Nagios
