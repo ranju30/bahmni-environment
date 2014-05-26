bbd11412c22993114e0ad316b6eeb0bd92e4a71f
Deepak N <endeep123@gmail.com>
2014-05-26 18:31:21 +0530

    Fix bahmni backup to create backup folder before cron tries to sync using relative path

--- puppet/modules/bahmni-backup/manifests/init.pp
+++ puppet/modules/bahmni-backup/manifests/init.pp
@@ -1,9 +1,19 @@
 class bahmni-backup {
+    exec { "create_patient_images_dir_on_secondary_machine":
+        command => "ssh root@${secondary_machine_ip} 'mkdir -p ${imagesDirectory}'",
+        path        => "${os_path}",
+    }
+
     #--delete option is taken out from rsync intentionally. As slave folder acts as backup.
     cron { "sync_patient_image_cron" :
         command => "rsync -rh --progress -i --itemize-changes --update --chmod=Du=r,Dg=rwx,Do=rwx,Fu=rwx,Fg=rwx,Fo=rwx -p ${imagesDirectory} -e ssh root@${secondary_machine_ip}:${imagesDirectory}/../",
         user    => "root",
         minute  => "*/1"
+    }
+
+    exec { "create_document_images_dir_on_secondary_machine":
+        command => "ssh root@${secondary_machine_ip} 'mkdir -p ${documentBaseDirectory}'",
+        path        => "${os_path}",
     }
 
 	cron { "sync_document_image_cron" :
