eff7efdab5de223ae9cce453948154251bc2d158
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-06-23 15:34:51 +0530

    Sush, Gurpreet| Added script for starting/stopping/backup/etc of bahmni.

--- scripts/bahmni.sh
+++ scripts/bahmni.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+
+PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "[Error] This script must be run as root or with sudo!" 1>&2
+   exit 1
+fi
+
+
+
 function is_service_running() {
 	if (( $(ps -ef | grep -v grep | grep $1 | wc -l) > 0 ))
 	then
@@ -18,6 +28,16 @@ function start_service() {
 		echo -e "Service $1 is already up"
 	fi
 }
+
+function start_pgsql() {
+	echo "Starting up postgresql-9.2..."
+	if(!(is_service_running pgsql-9.2 )) then
+		sudo service postgresql-9.2 start 
+	else
+		echo -e "Service postgresql-9.2 is already up"
+	fi
+}
+
 
 function stop_service() {
 	echo "Shutting down $1 service..."
@@ -41,6 +61,7 @@ stop() {
 		ps aux | grep [t]omcat | awk '{print $2}' | xargs -I PID sudo kill -9 PID
 	fi
 
+    sleep 3
 	stop_service mysql
 	stop_service postgresql-9.2
 	stop_service httpd
@@ -53,11 +74,15 @@ start() {
 	echo "Run [bahmni status] to check the status"
 	echo "=================================================================="
 	start_service mysql
-	start_service postgresql-9.2
+	start_pgsql
 	start_service httpd
 	sleep 3
 	start_service openerp
 	start_service tomcat
+	echo "=================================================================="
+	echo "Bahmni services started... "
+	echo "Tomcat will take upto 5 mins to fully come up...."
+	echo "=================================================================="
 }
 
 # restarts all services
@@ -67,11 +92,12 @@ restart() {
 	echo "Run [bahmni status] to check the status"
 	echo "=================================================================="
 	stop
+	sleep 3
 	start
 }
 
 status() {
-	services=("openerp-server" "tomcat" "mysql" "pgsql-9.2" "httpd")
+	services=("httpd" "openerp-server" "tomcat" "mysql" "pgsql-9.2")
 	# up_count=0
 	# down_count=0
 	for service in "${services[@]}" 
@@ -87,30 +113,19 @@ status() {
 }
 
 backup() {
-	case $1 in
-		"postgres" )
-			backup-pgsql $2
-			;;
-		"mysql" )
-			backup-mysql $2
-			;;
-		"all" )
-			backup-all-dbs $2 $3
-			;;
-		* )
-			echo "Invalid backup argument"
-			exit 1;
-			;;
-	esac
+	sudo $PATH_OF_CURRENT_SCRIPT/backup-all-dbs.sh -b /backup
 }
 
 tailFunction() {
 	case "$1" in
 		"access" )
-			tail -F $TOMCAT_HOME/logs/localhost_access*
+			sudo tail -f /var/log/httpd/access_log
 			;;
-		"catalina" )
-			tail -F $TOMCAT_HOME/logs/catalina.out
+		"tomcat" )
+			sudo tail -f /home/bahmni/apache-tomcat-7.0.22/logs/catalina.out
+			;;
+		"openerp" )
+			sudo tail -f /var/log/openerp/openerp-server.log
 			;;
 		* )
 			echo "Incorrect parameter for tail"
@@ -121,14 +136,16 @@ tailFunction() {
 }
 
 printUsage() {
+	echo -e "-----------------------------------------"
 	echo -e "Command line tool for managing bahmni"
 	echo -e "\nUsage:"
 	echo -e "\tbahmni start"
 	echo -e "\tbahmni stop"
 	echo -e "\tbahmni restart"
-	echo -e "\tbahmni tail [ tomcataccess | catalinaout ]"
-	echo -e "\tbahmni backup [ mysql | postgres | all] [location]"
+	echo -e "\tbahmni logs [ tomcat | access | openerp ]"
+	echo -e "\tbahmni backup-all-dbs"
 	echo -e "\tbahmni status"
+	echo -e "\n-----------------------------------------"
 }
 
 case "$1" in
@@ -144,14 +161,14 @@ case "$1" in
 	"restart" )
 		restart
 		;;
-	# "backup" )
-	# 	backup $2 $3 $4
-	# 	;;
-	# "tail" )
-	# 	tailFunction $2
-	# 	;;
+	"backup-all-dbs" )
+	    backup
+		;;
+	"logs" )
+	    tailFunction $2
+		;;
 	"status" )
-		status $2
+		status
 		;;
 	* )
 		echo "[ERROR] Invalid option $1"
