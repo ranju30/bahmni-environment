ed012f8b90fc0e5cb32ba39e7a6b61cc71868582
mihirk <mihir.khatwani@gmail.com>
2014-07-03 11:06:30 +0530

    Vinay, Mihir | Adding liquibase snapshot version 1. Supports OpenMRS upgrade 1.9.8 to 1.10

---
+++ puppet/modules/bahmni-snapshot-migrations/manifests/init.pp
@@ -0,0 +1,24 @@
+class bahmni-snapshot-migrations() {
+  file { "${temp_dir}/snapshots" :
+    ensure => "directory",
+    source => "puppet:///modules/bahmni-snapshot-migrations/snapshots",
+    owner => "${bahmni_user}",
+    recurse => "true",
+  }
+
+  file { "${temp_dir}/run-snapshot-liquibase.sh" :
+    ensure      => present,
+    content     => template("bahmni-snapshot-migrations/run-snapshot-liquibase.sh"),
+    owner       => "${bahmni_user}",
+    group       => "${bahmni_user}",
+    mode        => 554,
+    require => File["${temp_dir}/snapshots"]
+  }
+
+  exec { "run-snapshot-migrations" :
+    command     => "${temp_dir}/run-snapshot-liquibase.sh ${deployment_log_expression}",
+    path        => "${os_path}",
+    provider    => shell,
+    require => File["${temp_dir}/run-snapshot-liquibase.sh"]
+  }
+}
