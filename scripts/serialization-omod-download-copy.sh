79601df4315345b86ee4afe54bf7feb85c45465d
Vinay Venu <vinayv@thoughtworks.com>
2014-08-05 10:47:50 +0530

    Vinay | Remove copy script as it is no longer required. Serialization omod has been fixed now

--- scripts/serialization-omod-download-copy.sh
+++
@@ -1,4 +0,0 @@
-rm -f /home/jss/.OpenMRS/modules/serialization.xstream-0.2.8-SNAPSHOT.omod
-cp modules/serialization.xstream-0.2.8-SNAPSHOT.omod /home/jss/.OpenMRS/modules/serialization.xstream-0.2.8-SNAPSHOT.omod
-chown jss:jss /home/jss/.OpenMRS/modules/serialization.xstream-0.2.8-SNAPSHOT.omod 
-chmod 777 /home/jss/.OpenMRS/modules/serialization.xstream-0.2.8-SNAPSHOT.omod
