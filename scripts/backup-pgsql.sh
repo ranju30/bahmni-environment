0f9821c31881d6d38b7ceeafb056d4f3342d6e90
Vinkesh Banka <vinkesh.banka@gmail.com>
2014-08-28 17:51:30 +0530

    Banka | Minor change in pgsql backup

--- scripts/backup-pgsql.sh
+++ scripts/backup-pgsql.sh
@@ -5,4 +5,4 @@ set -e
 TIME=`date +%Y%m%d_%H%M%S`
 BACKUP_DIR=${1:-/backup}
 
-su -c "pg_dumpall -U postgres | gzip -c > ${BACKUP_DIR}/pgsql_backup_$TIME.sql.gz"
+pg_dumpall -U postgres | gzip -c > ${BACKUP_DIR}/pgsql_backup_$TIME.sql.gz
