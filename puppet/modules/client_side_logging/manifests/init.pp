1ccd1eb05b878bb99447a1d5d7d4901402355642
mihirk <mihir.khatwani@gmail.com>
2014-09-23 11:24:02 +0530

    Mihir | Fixing flask install

--- puppet/modules/client_side_logging/manifests/init.pp
+++ puppet/modules/client_side_logging/manifests/init.pp
@@ -1,8 +1,11 @@
 class client_side_logging() {
   package { "mod_wsgi" : ensure => "installed" }
-
+  $python_temp_dir = "${temp_dir}/python"
+  $os_path = "/sbin:/bin:/usr/sbin/:/usr/bin:/usr/local/bin:/usr/local/sbin"
   exec { "flask" :
     command => "easy_install flask",
+    path => "${os_path}",
+    cwd => "${python_temp_dir}",
   }
 
   file { "${python_site_packages}/${client_side_logging}" :
