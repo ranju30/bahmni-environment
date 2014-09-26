549d0627ac7c745e2adab27bd0e5ccdd70e78c19
Deepak N <endeep123@gmail.com>
2014-09-26 16:04:25 +0530

    Fix implementation_config module to deploy required apps only

--- puppet/modules/implementation_config/manifests/init.pp
+++ puppet/modules/implementation_config/manifests/init.pp
@@ -1,5 +1,9 @@
 class implementation_config($implementation_name = $implementation_name) {
   class {"implementation_config::openmrs": implementation_name => "$implementation_name"}
-  class {"implementation_config::openerp": implementation_name => "$implementation_name"}
-  class {"implementation_config::openelis": implementation_name => "$implementation_name"}
+  if $deploy_bahmni_openelis == "true" {
+	  class {"implementation_config::openelis": implementation_name => "$implementation_name"}
+  }
+  if $deploy_bahmni_openerp == "true" {
+	  class {"implementation_config::openerp": implementation_name => "$implementation_name"}
+  }
 }
