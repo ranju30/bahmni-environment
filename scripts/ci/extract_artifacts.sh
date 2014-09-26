45ea23a2e2c22bf9e7318679eba9894dae9604e4
rohanpoddar <rohan.poddar@gmail.com>
2014-09-26 10:45:03 +0530

    Rohan, Vinay | #811 | Created a script to extract artifacts in QA for deployment

---
+++ scripts/ci/extract_artifacts.sh
@@ -0,0 +1,19 @@
+IMPLEMENTATION_NAME=$1
+
+if [ Z${IMPLEMENTATION_NAME} = "Z" ] 
+then
+	echo "Argument required"
+	exit 1
+fi
+
+mv -f deployables/mrs/* /packages/build/
+mv -f deployables/erp/* /packages/build/
+mv -f deployables/elis/* /packages/build/
+mv -f deployables/referencedata/* /packages/build/
+mv -f deployables/environment/* /packages/build/
+mv -f deployables/${IMPLEMENTATION_NAME}_config.zip /packages/build
+
+rm -rf deployables
+
+
+rm -f /packages/build/*_md5.checksum
