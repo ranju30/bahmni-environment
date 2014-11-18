92f84b7ea552c2593520cd0d6b3461b745adc224
mihirk <mihir.khatwani@gmail.com>
2014-11-18 23:21:31 +0530

    Mihir | Adding lokbiradari reports as key value pair in the reports list

--- puppet/manifests/configurations/stack-runtime-configuration.pp
+++ puppet/manifests/configurations/stack-runtime-configuration.pp
@@ -163,7 +163,8 @@ $report_zip_source_url = $implementation_name ? {
 	  undef			 => "https://github.com/jss-emr/jss-reports/archive/master.zip",
       "jss"			 => "https://github.com/jss-emr/jss-reports/archive/master.zip",
       default        => "https://github.com/jss-emr/jss-reports/archive/master.zip",
-      "search"       => "https://github.com/Bhamni/search-reports/archive/master.zip"
+      "search"       => "https://github.com/Bhamni/search-reports/archive/master.zip",
+      "lokbiradari"  => "https://github.com/Bhamni/lokbiradari-reports/archive/master.zip"
 }
 
 ######################## JASPER CONFIG END##############################################
