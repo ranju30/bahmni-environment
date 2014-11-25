b8e9375519ef8c7a4cb46fe4f41a8f43e3af0d6a
Hemanth S.R <hemanthfeb25@gmail.com>
2014-11-25 14:51:02 +0530

    Move implementation_config set up to individual app

--- puppet/modules/implementation_config/manifests/openerp.pp
+++ puppet/modules/implementation_config/manifests/openerp.pp
@@ -1,5 +1,7 @@
 class implementation_config::openerp {
-  implementation_config::migrations { "implementation_config_migrations_openerp":
+ require implementation_config::setup
+  
+ implementation_config::migrations { "implementation_config_migrations_openerp":
     implementation_name => "${implementation_name}",
     app_name            => "openerp"
   }
