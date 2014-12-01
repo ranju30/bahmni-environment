265691e266ff3ca69b9d1315650bb3b3d609c533
rohanpoddar <rohan.poddar@gmail.com>
2014-12-01 17:46:10 +0530

    Rohan, Vinay | #816 | Removed implementation config from the all deploy script

--- scripts/ci/bahmni_deploy.sh
+++ scripts/ci/bahmni_deploy.sh
@@ -3,7 +3,6 @@
 erp_installer_file=erp_installer.sh
 elis_installer_file=elis_installer.sh
 mrs_installer_file=mrs_installer.sh
-implementation_installer_file=$1
 
 TEMP_SCRIPT_DIR=`dirname -- "$0"`
 SCRIPT_DIR=`cd $TEMP_SCRIPT_DIR; pwd`
@@ -12,9 +11,5 @@ export SCRIPT_DIR
 sh $SCRIPT_DIR/$erp_installer_file --target /packages/build
 sh $SCRIPT_DIR/$elis_installer_file --target /packages/build
 sh $SCRIPT_DIR/$mrs_installer_file --target /packages/build
-
-if [ -f $SCRIPT_DIR/$implementation_installer_file ]; then
-	sh $implementation_installer_file --target /packages/build
-fi
 
 echo "Installation Complete"
