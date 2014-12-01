7a37a189497de94074e36903fdcbc10fc5768ae5
rohanpoddar <rohan.poddar@gmail.com>
2014-12-01 10:56:36 +0530

    Rohan | #816 | Created a separate installer for implementation configs

--- scripts/ci/bahmni_deploy.sh
+++ scripts/ci/bahmni_deploy.sh
@@ -3,6 +3,7 @@
 erp_installer_file=$1
 elis_installer_file=$2
 mrs_installer_file=$3
+implementation_installer_file=$4
 
 TEMP_SCRIPT_DIR=`dirname -- "$0"`
 SCRIPT_DIR=`cd $TEMP_SCRIPT_DIR; pwd`
@@ -26,5 +27,9 @@ fi
 sh $SCRIPT_DIR/$erp_installer_file
 sh $SCRIPT_DIR/$elis_installer_file
 sh $SCRIPT_DIR/$mrs_installer_file
+
+if [ -f $SCRIPT_DIR/$implementation_installer_file ]; then
+	sh $SCRIPT_DIR/$implementation_installer_file
+fi
 
 echo "Installation Complete"
