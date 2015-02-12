64bcf24251a476cc3b800e7bf14db6e35f5df925
mihirk <mihir.khatwani@gmail.com>
2015-02-12 21:50:12 +0530

    Mihir | Adding time to backup folder

--- scripts/ci/backup-all-dbs.sh
+++ scripts/ci/backup-all-dbs.sh
@@ -8,6 +8,7 @@ sudo mkdir -pv $BACKUP_PATH
 # Perform backup of MYSQL DB
 TIME=`date +%Y%m%d_%H%M%S`
 $BACKUP_PATH="$BACKUP_PATH/backup_$TIME"
+sudo mkdir -pv $BACKUP_PATH
 sudo mysqldump -uroot -ppassword --all-databases --routines | gzip > $BACKUP_PATH/mysql_backup_$TIME.sql.gz
 
 sudo chown postgres:postgres $BACKUP_PATH
