dd2091fa2dce594a8d27955bc8366b4210ad6d91
mihirk <mihir.khatwani@gmail.com>
2015-02-12 21:54:12 +0530

    Mihir | Fixing mistake

--- scripts/ci/backup-all-dbs.sh
+++ scripts/ci/backup-all-dbs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e -x
 
-BACKUP_PATH='/backup'
+ALL_BACKUP_PATH='/backup'
 echo "Attempting to create backup folder: $BACKUP_PATH"
-sudo mkdir -pv $BACKUP_PATH
+sudo mkdir -pv $ALL_BACKUP_PATH
 
 # Perform backup of MYSQL DB
 TIME=`date +%Y%m%d_%H%M%S`
-$BACKUP_PATH="$BACKUP_PATH/backup_$TIME"
+BACKUP_PATH="$ALL_BACKUP_PATH/backup_$TIME"
 sudo mkdir -pv $BACKUP_PATH
 sudo mysqldump -uroot -ppassword --all-databases --routines | gzip > $BACKUP_PATH/mysql_backup_$TIME.sql.gz
 
