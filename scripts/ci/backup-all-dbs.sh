b187714ed43b7faf32ae657955f38e03db40475f
mihirk <mihir.khatwani@gmail.com>
2015-02-12 21:29:20 +0530

    Mihir | Adding hardcoded backup DBs script

---
+++ scripts/ci/backup-all-dbs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e -x
+
+BACKUP_PATH='/backup'
+echo "Attempting to create backup folder: $BACKUP_PATH"
+sudo mkdir -pv $BACKUP_PATH
+
+# Perform backup of MYSQL DB
+TIME=`date +%Y%m%d_%H%M%S`
+$BACKUP_PATH="$BACKUP_PATH/backup_$TIME"
+sudo mysqldump -uroot -ppassword --all-databases --routines | gzip > $BACKUP_PATH/mysql_backup_$TIME.sql.gz
+
+chown postgres:postgres $BACKUP_PATH
+
+# Perform backup of PostgreSQL DB
+su - postgres -c "pg_dumpall | gzip -c > $BACKUP_PATH/pgsql_backup_$TIME.sql.gz"
+
+echo ">> Backups created at: $BACKUP_PATH"
