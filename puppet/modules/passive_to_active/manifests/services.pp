24631724f816a1c4fdde2d6a49ee8183014ed2e0
Hemanth S.R <hemanthfeb25@gmail.com>
2015-02-24 15:07:50 +0530

    V!kas, He-man | checking if the implementation is using openerp before start up while switching.

--- puppet/modules/passive_to_active/manifests/services.pp
+++ puppet/modules/passive_to_active/manifests/services.pp
@@ -5,23 +5,24 @@ class passive_to_active::services{
   notice("starting httpd...")
   service { "httpd":
     enable    => true,
-    ensure => running
+    ensure    => running
   }
 
   notice("starting tomcat...")
-  exec{"start-tomcat":
-    command => "service tomcat start",
-    user => "root",
-    path => "${os_path}",
+  exec{ "start-tomcat":
+    command  => "service tomcat start",
+    user     => "root",
+    path     => "${os_path}",
     provider => "shell"
   }
 
-  notice("starting openerp...")
-  exec{"start-openerp":
-    command => "service openerp start",
-    user => "root",
-    path => "${os_path}",
-    provider => "shell"
+  if $bahmni_openerp_required == "true" {
+    notice("starting openerp...")
+    exec{ "start-openerp":
+      command  => "service openerp start",
+      user     => "root",
+      path     => "${os_path}",
+      provider => "shell"
+    }
   }
-
 }
