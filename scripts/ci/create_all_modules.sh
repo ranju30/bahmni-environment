1ea893b5960ea06aac67e87fbc20803efd0b5db0
bharatak <bharatak@thoughtworks.com>
2015-02-27 13:07:21 +0530

    Bharat| Updated create_all_modules.sh to build the all_installer.sh

--- scripts/ci/create_all_modules.sh
+++ scripts/ci/create_all_modules.sh
@@ -4,10 +4,15 @@ SCRIPT_DIR=`cd $TEMP_SCRIPT_DIR; pwd`
 export SCRIPT_DIR
 export IMPLEMENTATION=$1
 
+cp bahmni-environment.zip deployables
+cp bahmni-environment.zip erp/deployables
+cp bahmni-environment.zip elis/deployables
+unzip -qo bahmni-environment.zip -d erp/deployables
+
 sh $SCRIPT_DIR/create_module_installer.sh deployables mrs_installer.sh "MRS Installer" bahmni_openmrs
 sh $SCRIPT_DIR/create_module_installer.sh deployables-implementation $IMPLEMENTATION"_config_installer.sh" "Implementation Config Installer" implementation_config::openmrs
-sh $SCRIPT_DIR/create_module_installer.sh deployables-elis elis_installer.sh "Elis Installer" bahmni_openelis
-sh $SCRIPT_DIR/create_module_installer.sh deployables-erp erp_installer.sh "ERP Installer" bahmni_openerp
+sh $SCRIPT_DIR/create_module_installer.sh elis/deployables elis_installer.sh "Elis Installer" bahmni_openelis
+sh $SCRIPT_DIR/create_module_installer.sh erp/deployables erp_installer.sh "ERP Installer" bahmni_openerp
 
 rm -rf final
 mkdir -p final
