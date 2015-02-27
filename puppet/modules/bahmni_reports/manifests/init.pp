5066547b5e1eaf490bf8f73f624a1da53719f98a
chethanTw <chethandec22@gmail.com>
2015-02-27 10:37:37 +0530

    Chethan, Sravanthi | #1749 | Deployment of Bahmni Reports

--- puppet/modules/bahmni_reports/manifests/init.pp
+++ puppet/modules/bahmni_reports/manifests/init.pp
@@ -1,4 +1,10 @@
 class bahmni_reports {
+  $bahmni_reports_dir = "${build_output_dir}/bahmni-enviroment/puppet/modules/bahmni_reports"
+  $webapps_dir="${tomcatInstallationDirectory}/webapps"
+  $liquibase_jar="${webapps_dir}/bahmnireports/WEB-INF/lib/liquibase-core-2.0.5.jar"
+  $bahmni_reports_war_path="${build_output_dir}/${bahmni_reports_war_file_name}.war"
+  $mysql_driver_path="${packages_servers_dir}/mysql-connector-java-${mysql_connector_java_version}.jar"
+
 	exec { "clear_bahmni_reports_webapp" :
 	    command   => "rm -rf ${tomcatInstallationDirectory}/webapps/${bahmni_reports_war_file_name}",
 	    provider  => shell,
@@ -7,10 +13,25 @@ class bahmni_reports {
 	}
 
     exec { "latest_bahmni_reports_webapp" :
-	    command   => "unzip -o -q ${build_output_dir}/${bahmni_reports_war_file_name}.war -d ${tomcatInstallationDirectory}/webapps/${bahmni_reports_war_file_name} ${deployment_log_expression}",
+	    command   => "unzip -o -q ${build_output_dir}/${bahmni_reports_war_file_name}.war -d ${webapps_dir}/${bahmni_reports_war_file_name} ${deployment_log_expression}",
 	    provider  => shell,
 	    path      => "${os_path}",
 	    require   => [Exec["clear_bahmni_reports_webapp"]],
 	    user      => "${bahmni_user}"
 	}
+
+	file { "${temp_dir}/run-bahmni-reports-liquibase.sh" :
+        ensure      => present,
+        content     => template("bahmni_reports/run-bahmni-reports-liquibase.sh"),
+        owner       => "${bahmni_user}",
+        group       => "${bahmni_user}",
+        mode        => 554
+      }
+
+    exec { "run_bahmni_reports_liquibase" :
+      command     => "${temp_dir}/run-bahmni-reports-liquibase.sh ${deployment_log_expression}",
+      path        => "${os_path}",
+      provider    => shell,
+      require     => [Exec["latest_bahmni_reports_webapp"],File["${temp_dir}/run-bahmni-reports-liquibase.sh"]]
+    }
 }
