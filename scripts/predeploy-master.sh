5fa7734f837f6fde4811f2e1f79c20964a101080
mihirk <mihir.khatwani@gmail.com>
2015-03-22 18:42:36 +0545

    Mihir| Changing predeploy script user to Bahmni

--- scripts/predeploy-master.sh
+++ scripts/predeploy-master.sh
@@ -28,8 +28,8 @@ fi
 sh $SCRIPTS_DIR/backup-mysql.sh $mysqlRootPassword $BACKUP_DIR
 sh $SCRIPTS_DIR/backup-pgsql.sh $BACKUP_DIR
 
-cp -r /home/jss/.OpenMRS $BACKUP_DIR/
+cp -r /home/bahmni/.OpenMRS $BACKUP_DIR/
 cp -r /packages $BACKUP_DIR/
-cp -r /home/jss/apache-tomcat-8.0.12/webapps $BACKUP_DIR/
+cp -r /home/bahmni/apache-tomcat-8.0.12/webapps $BACKUP_DIR/
 cp -r /etc/httpd $BACKUP_DIR/
 cp -r /usr/lib/python2.6/site-packages $BACKUP_DIR/
