b82b9029ec60db44c5b746440997f7f420d4a3bb
Divya <divyadogra2906@gmail.com>
2015-04-03 12:04:07 +0530

    Divya | Installing Jdk always for jasper reports to work

--- puppet/modules/java/manifests/init.pp
+++ puppet/modules/java/manifests/init.pp
@@ -1,44 +1,17 @@
 class java {
   require yum_repo
-  
-  if ($install_jdk == "true") {
-    $java_home = "/usr/java/jdk1.7.0_21"
 
-    package { "jdk" :
-      ensure => installed
-    }
-    
-    file { "jdk_home_path" :
-      path    => "/etc/profile.d/java.sh",
-      ensure  => present,
-      content => template ("java/java.sh"),
-      mode    => 664,
-      require => Package["jdk"]
-    }
+  $java_home = "/usr/java/jdk1.7.0_21"
 
-  } else {
+  package { "jdk" :
+    ensure => installed
+  }
 
-    $java_home = "/usr/java/default"
-
-    package { "jre" :
-      ensure => installed
-    }
-    
-    file { "java_home_path" :
-      path    => "/etc/profile.d/java.sh",
-      ensure  => present,
-      content => template ("java/java.sh"),
-      mode    => 664,
-      require => Package["jre"]
-    }
-
-    file { "${java_home}/lib/tools.jar" :
-      source => "${packages_servers_dir}/tools.jar",
-      ensure => present,
-      require => Package["jre"]
-    }
-
-}
-
-
+  file { "jdk_home_path" :
+    path    => "/etc/profile.d/java.sh",
+    ensure  => present,
+    content => template ("java/java.sh"),
+    mode    => 664,
+    require => Package["jdk"]
+  }
 }
