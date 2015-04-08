1ecfd7f9cc9cfae2bb20f2da9b640de317d6444b
rohanpoddar <rohan.poddar@gmail.com>
2015-04-08 16:37:42 +0530

    Rohan | #000 | Fixing the provision script

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Ideally everything should be installed via puppet but for puppet to run certain utilities need to be installed for puppet to run
+
+SCRIPTS_DIR=`dirname $0`
+
 sudo yum -y install ruby
 sudo rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
 sudo yum -y install puppet
@@ -20,4 +23,4 @@ fi
 sudo chmod -R 777 /packages
 sudo sed -i '/^127\.0\.0\.1/ s/$/'" $HOSTNAME"'/' /etc/hosts
 
-sh run-puppet-manifest.sh provision
+sh $SCRIPTS_DIR/run-puppet-manifest.sh provision
