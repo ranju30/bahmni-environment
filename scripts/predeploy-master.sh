c01429ded11997b957b80932184a7e74199910fe
Vinkesh Banka <vinkesh.banka@gmail.com>
2015-04-10 23:43:23 +0530

    Banka | Using environment variable to determise home directory of bahmni user

--- scripts/predeploy-master.sh
+++ scripts/predeploy-master.sh
@@ -10,6 +10,7 @@ if [ -z $mysqlRootPassword ]; then
 fi
 
 SCRIPTS_DIR=`dirname $0`
+BAHMNI_USER_NAME=${FACTER_bahmni_user_name:-bahmni}
 
 TIME=`date +%Y%m%d_%H%M%S`
 
@@ -28,8 +29,8 @@ fi
 sh $SCRIPTS_DIR/backup-mysql.sh $mysqlRootPassword $BACKUP_DIR
 sh $SCRIPTS_DIR/backup-pgsql.sh $BACKUP_DIR
 
-cp -r /home/bahmni/.OpenMRS $BACKUP_DIR/
+cp -r /home/$BAHMNI_USER_NAME/.OpenMRS $BACKUP_DIR/
 cp -r /packages $BACKUP_DIR/
-cp -r /home/bahmni/apache-tomcat-8.0.12/webapps $BACKUP_DIR/
+cp -r /home/$BAHMNI_USER_NAME/apache-tomcat-8.0.12/webapps $BACKUP_DIR/
 cp -r /etc/httpd $BACKUP_DIR/
 cp -r /usr/lib/python2.6/site-packages $BACKUP_DIR/
