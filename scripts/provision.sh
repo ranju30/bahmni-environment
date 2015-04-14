472bd162eb90d331b2bf5cea79dbdd9db2fe0a8a
Vinay Venu <vinayv@thoughtworks.com>
2015-04-14 15:16:09 +0530

    Sandeep, Vinay | Ensure directories are created correctly. Remember that / does not work with wget, hence the use of /./

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -10,15 +10,9 @@ sudo mkdir /packages
 if [[ $FACTER_go_bahmni_yum_repo_url != '' ]]
 then
         echo $FACTER_go_bahmni_yum_repo_url
-        sudo wget -r -nH --no-parent --reject="index.html*" http://$FACTER_go_bahmni_yum_repo_url/packages/localrepo/ -P /packages
-        sudo wget -r -nH --no-parent --reject="index.html*" http://$FACTER_go_bahmni_yum_repo_url/packages/tools/ -P /packages
-        sudo wget -r -nH --no-parent --reject="index.html*" http://$FACTER_go_bahmni_yum_repo_url/packages/servers/ -P /packages
-        sudo wget -r -nH --no-parent --reject="index.html*" http://$FACTER_go_bahmni_yum_repo_url/packages/python-packages/ -P /packages
+		sudo wget -r -nH --no-parent --reject="index.html*" http://$FACTER_go_bahmni_yum_repo_url/packages/ -P /./
 else
-        sudo wget -r -nH --no-parent --reject="index.html*" https://bahmni-repo.twhosted.com/packages/localrepo/ -P /packages
-        sudo wget -r -nH --no-parent --reject="index.html*" https://bahmni-repo.twhosted.com/packages/tools/ -P /packages
-        sudo wget -r -nH --no-parent --reject="index.html*" https://bahmni-repo.twhosted.com/packages/servers/ -P /packages
-        sudo wget -r -nH --no-parent --reject="index.html*" https://bahmni-repo.twhosted.com/packages/python-packages/ -P /packages
+        sudo wget -r -nH --no-parent --reject="index.html*" https://bahmni-repo.twhosted.com/packages/ -P /./
 fi
 sudo chmod -R 777 /packages
 sudo sed -i '/^127\.0\.0\.1/ s/$/'" $HOSTNAME"'/' /etc/hosts
