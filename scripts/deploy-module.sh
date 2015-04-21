b6e87af725ca0854cd0f6c31fb16f3cda2a82430
Vinay Venu <vinayv@thoughtworks.com>
2015-04-21 16:29:27 +0530

    Ensure you use 1.8.7 via rvm. This is temporary, to find if it will fix functional tests

--- scripts/deploy-module.sh
+++ scripts/deploy-module.sh
@@ -32,6 +32,7 @@ then
 	exit 1
 fi
 
+rvm use `rvm list versions | grep 1.8.7 | head -1`
 FACTER_module_to_run=$MODULE_NAME puppet apply $BASE_DIR/puppet/manifests/run.pp  --modulepath=$BASE_DIR/puppet/modules/  --detailed-exitcodes
 RETURN_CODE=$?
 if [ $RETURN_CODE -ne 0 ] && [ $RETURN_CODE -ne 2 ]
