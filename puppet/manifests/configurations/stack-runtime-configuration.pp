c35f9a8c3db9e4f9b1427a4bd1677bc80cc0ec89
arunpaul <arunpaul@INarunpaul1.local>
2015-05-05 15:01:51 +0530

    Added default ip address for active machine and passive machine

--- puppet/manifests/configurations/stack-runtime-configuration.pp
+++ puppet/manifests/configurations/stack-runtime-configuration.pp
@@ -39,7 +39,10 @@ $reports_environment = $bahmni_reports_environment ? {
 }
 
 # Machines
-$active_machine_ip = $bahmni_active_machine_ip 
+$active_machine_ip = $bahmni_active_machine_ip ? {
+  undef     => "127.0.0.1",
+  default       => $bahmni_active_machine_ip
+}
 $active_machine_host_name = $bahmni_active_machine_host_name ? {
   undef     => "emr01",
   default       => $bahmni_active_machine_host_name
@@ -48,8 +51,10 @@ $active_machine_alias = $bahmni_active_machine_alias ? {
   undef     => "emr01alias",
   default       => $bahmni_active_machine_alias
 }
-
-$passive_machine_ip = $bahmni_passive_machine_ip 
+$passive_machine_ip = $bahmni_passive_machine_ip ? {
+  undef     => "127.0.0.1",
+  default       => $bahmni_passive_machine_host_name
+}
 $passive_machine_host_name = $bahmni_passive_machine_host_name ? {
   undef     => "passivehost",
   default       => $bahmni_passive_machine_host_name
