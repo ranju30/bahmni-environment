6465584267735e27c046686f2127d6dabe2c19ff
shruthidipali <shruthidipali@gmail.com>
2015-05-11 18:41:27 +0530

    Hemanth, Shruthi | Adding provision script for nagios server

---
+++ puppet/manifests/provision-nagios-server.pp
@@ -0,0 +1,8 @@
+import "configurations/node-configuration"
+import "configurations/stack-installers-configuration"
+import "configurations/stack-runtime-configuration"
+import "configurations/deployment-configuration"
+
+node default {
+  include bahmni_nagios_server
+}
