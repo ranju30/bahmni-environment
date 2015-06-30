5b394e2b96d48a6d45d296c4c80c00c99d6253d2
Hemanth S.R <hemanthfeb25@gmail.com>
2015-06-30 17:20:57 +0530

    Sandeep, Hemanth | puppet module to install pacs integration.

--- puppet/modules/pacs/manifests/init.pp
+++ puppet/modules/pacs/manifests/init.pp
@@ -1,12 +1,9 @@
 class pacs {
   require tomcat::clean
-  include bahmni_revisions
-  include bahmni_configuration
 
-  $pacs_webapp_location =  "${tomcatInstallationDirectory}/webapps/pacs"
+  $pacs_webapp_location =  "${tomcatInstallationDirectory}/webapps/pacs-integration"
   $bahmni_pacs_temp_dir = "${temp_dir}/PACS"
   $log4j_xml_file = "${tomcatInstallationDirectory}/webapps/${$pacs_war_file_name}/WEB-INF/classes/log4j.xml"
-
 
   file { "${pacs_webapp_location}" : ensure => absent, purge => true}
 
@@ -18,15 +15,6 @@ class pacs {
     user      => "${bahmni_user}",
   }
 
-  file { "${bahmni_pacs_temp_dir}" : ensure => absent, purge => true}
-
-  exec { "bahmni_pacs_codebase" :
-    provider => "shell",
-    command   => "unzip -o -q ${build_output_dir}/pacs-integration.zip -d ${temp_dir} ${deployment_log_expression}",
-    path => "${os_path}",
-    require   => [File["${bahmni_pacs_temp_dir}"]]
-  }
-
   file { "${log4j_xml_file}" :
     ensure      => present,
     content     => template("pacs/log4j.xml.erb"),
@@ -35,26 +23,22 @@ class pacs {
     require     => Exec["latest_pacs_webapp"],
     mode        => 664,
   }
-
-  if $is_passive_setup == "false" {
-    include pacs::database
-  }
 }
 
 class pacs::database {
   $bahmni_pacs_temp_dir = "${temp_dir}/PACS"
 
-  file { "${bahmni_pacs_temp_dir}/scripts/initDB.sh":
-    ensure => present,
-    mode   => 755,
-    require => [Exec["bahmni_pacs_codebase"]]
+  file { "${temp_dir}/create-pacs-db-and-user.sql" :
+    ensure      => present,
+    content     => template("bahmni_snapshot_migrations/database.sql"),
+    owner       => "${bahmni_user}",
+    group       => "${bahmni_user}"
   }
 
-  exec { "pacs_initdb" :
-    provider    => "shell",
-    cwd         => "${bahmni_pacs_temp_dir}",
-    command     => "scripts/initDB.sh",
-    require     => [File["${bahmni_pacs_temp_dir}/scripts/initDB.sh"]]
+  exec { "pacs_database" :
+    command     => "psql -Upostgres < ${temp_dir}/create-pacs-db-and-user.sql ${deployment_log_expression}",
+    path        => "${os_path}",
+    provider    => shell,
+    require     => File["${temp_dir}/create-pacs-db-and-user.sql"]
   }
-
 }
