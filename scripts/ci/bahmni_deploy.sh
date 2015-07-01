0b9cfd6b39b982ad163960897674ea340c115c98
Hemanth S.R <hemanthfeb25@gmail.com>
2015-07-01 11:51:56 +0530

    Preethi, Hemanth | installing pacs as part of all installer.

--- scripts/ci/bahmni_deploy.sh
+++ scripts/ci/bahmni_deploy.sh
@@ -4,6 +4,7 @@ erp_installer_file=erp_installer.sh
 elis_installer_file=elis_installer.sh
 mrs_installer_file=mrs_installer.sh
 bahmni_reports_installer_file=bahmni_reports_installer.sh
+bahmni_pacs_installer_file=bahmni_pacs_installer.sh
 
 TEMP_SCRIPT_DIR=`dirname -- "$0"`
 SCRIPT_DIR=`cd $TEMP_SCRIPT_DIR; pwd`
@@ -15,6 +16,8 @@ sh $SCRIPT_DIR/$elis_installer_file
 sh $SCRIPT_DIR/$mrs_installer_file
 
 sh $SCRIPT_DIR/$bahmni_reports_installer_file
+
+sh $SCRIPT_DIR/bahmni_pacs_installer_file
 
 if [ -f $SCRIPT_DIR/$erp_installer_file ]; then
     sh $SCRIPT_DIR/$erp_installer_file --target /packages/build
