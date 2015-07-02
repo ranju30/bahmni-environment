729a1b30fc0737e92d1aae2c759f965897ea1004
Hemanth S.R <hemanthfeb25@gmail.com>
2015-07-02 14:21:52 +0530

    Sudhakar, Hemanth | Merged the creation of pacs db into deployment task.

--- puppet/modules/bahmni_pacs/manifests/init.pp
+++ puppet/modules/bahmni_pacs/manifests/init.pp
@@ -1,12 +1,15 @@
 class bahmni_pacs {
+  $tomcat_webapp_location = "${tomcatInstallationDirectory}/webapps"
+  $pacs_webapp_location =  "${$tomcat_webapp_location}/pacs-integration"
+  $log4j_xml_file = "${tomcatInstallationDirectory}/webapps/${$pacs_war_file_name}/WEB-INF/classes/log4j.xml"
+
   if ($bahmni_pacs_required == "true") {
+
+    if $is_passive_setup == "false" {
+      require bahmni_pacs::database
+    }
+
     require tomcat::clean
-
-    $tomcat_webapp_location = "${tomcatInstallationDirectory}/webapps"
-    $pacs_webapp_location =  "${$tomcat_webapp_location}/pacs-integration"
-
-    $bahmni_pacs_temp_dir = "${temp_dir}/PACS"
-    $log4j_xml_file = "${tomcatInstallationDirectory}/webapps/${$pacs_war_file_name}/WEB-INF/classes/log4j.xml"
 
     exec { "clear_bahmni_pacs_webapp" :
       command   => "rm -rf ${tomcatInstallationDirectory}/webapps/${pacs_war_file_name}",
@@ -25,13 +28,32 @@ class bahmni_pacs {
 
     file { "${log4j_xml_file}" :
       ensure      => present,
-      content     => template("pacs/log4j.xml.erb"),
+      content     => template("bahmni_pacs/log4j.xml.erb"),
       owner       => "${bahmni_user}",
       group       => "${bahmni_user}",
       require     => Exec["latest_pacs_webapp"],
       mode        => 664,
     }
+
   } else {
     notice ("Not installing PACS Module. ")
+  }
+}
+
+class bahmni_pacs::database {
+  $bahmni_pacs_temp_dir = "${temp_dir}/pacs-integration"
+
+  exec { "bahmni_pacs_codebase" :
+    provider  => "shell",
+    command   => "unzip -o -q ${build_output_dir}/pacs-integration.war -d ${temp_dir}/${pacs_war_file_name} ${deployment_log_expression}",
+    path      => "${os_path}"
+  }
+
+  exec { "${$bahmni_pacs_temp_dir}/datasetup/initDB.sh" :
+    command     => "sh ${$bahmni_pacs_temp_dir}/datasetup/initDB.sh ${deployment_log_expression}",
+    provider    => shell,
+    path        => "${os_path}",
+    user        => "${bahmni_user}",
+    require     => Exec["bahmni_pacs_codebase"],
   }
 }
