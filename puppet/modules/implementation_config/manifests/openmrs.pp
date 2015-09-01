42ca15e36a7e44630102029ed7dc1758eb8699f1
hemanths <hemanths@INhemanths.local>
2015-09-01 12:18:37 +0530

    Revert "Hemanth | copying the elis feed interceptor folder to .Openmrs folder during deployment."
    This reverts commit 108975bcd9543daf2ed7aa6440cc56274376bb75.

--- puppet/modules/implementation_config/manifests/openmrs.pp
+++ puppet/modules/implementation_config/manifests/openmrs.pp
@@ -1,19 +1,17 @@
-class implementation_config::openmrs {
+  class implementation_config::openmrs {
   $openmrs_dir = "/home/${bahmni_user}/.OpenMRS"
   $obscalculator_dir = "${build_output_dir}/${implementation_name}_config/openmrs/obscalculator"
   $patient_matching_algorithm_dir = "${build_output_dir}/${implementation_name}_config/openmrs/patientMatchingAlgorithm"
   $ordertemplates_dir = "${build_output_dir}/${implementation_name}_config/openmrs/ordertemplates"
   $encounter_modifier_scripts_dir = "${build_output_dir}/${implementation_name}_config/openmrs/encounterModifier"
-  $elis_interceptor_scripts_dir = "${build_output_dir}/${implementation_name}_config/openmrs/elisFeedInterceptor"
-
-
+  
   require implementation_config::setup
 
   file { "/tmp/mysql-connector-java-5.1.28.jar" :
-    ensure  => "directory",
-    source  => "puppet:///modules/implementation_config/mysql-connector-java-5.1.28.jar",
-    owner   => "${bahmni_user}",
-    recurse => "true",
+      ensure  => "directory",
+      source  => "puppet:///modules/implementation_config/mysql-connector-java-5.1.28.jar",
+      owner   => "${bahmni_user}",
+      recurse => "true",
   }
 
   exec { "copyObsCalculatorToOpenMRSFolder" :
@@ -37,13 +35,6 @@ class implementation_config::openmrs {
     onlyif    => "test -d ${encounter_modifier_scripts_dir}"
   }
 
-  exec { "copyElisInterceptorScriptsToOpenMRSFolder" :
-    command   => "cp -rf ${elis_interceptor_scripts_dir} ${openmrs_dir}",
-    provider  => shell,
-    path      => "${os_path}",
-    onlyif    => "test -d ${elis_interceptor_scripts_dir}"
-  }
-
   exec { "copyordertemplatesToOpenMRSFolder" :
     command   => "cp -rf ${ordertemplates_dir} ${openmrs_dir}",
     provider  => shell,
@@ -53,9 +44,9 @@ class implementation_config::openmrs {
 
   exec { "changeOwnerAndPermissionForScriptFolders" :
     provider => "shell",
-    command  => "chown -R ${bahmni_user}:${bahmni_user} ${openmrs_dir}; chmod -R 775 ${openmrs_dir}",
-    path     => "${os_path}",
-    require  => [Exec["copyObsCalculatorToOpenMRSFolder"], Exec["copyPatientMatchingAlgorithmToOpenMRSFolder"], Exec["copyEncounterModifierScriptsToOpenMRSFolder"], Exec["copyordertemplatesToOpenMRSFolder"]]
+    command => "chown -R ${bahmni_user}:${bahmni_user} ${openmrs_dir}; chmod -R 775 ${openmrs_dir}",
+    path => "${os_path}",
+    require => [Exec["copyObsCalculatorToOpenMRSFolder"], Exec["copyPatientMatchingAlgorithmToOpenMRSFolder"], Exec["copyEncounterModifierScriptsToOpenMRSFolder"], Exec["copyordertemplatesToOpenMRSFolder"]]
   }
 
   implementation_config::migrations { "implementation_config_migrations_openmrs":
@@ -71,15 +62,15 @@ class implementation_config::openmrs {
 
   exec { "set_owner_of_bahmni_config" :
     provider => "shell",
-    command  => "chown -R ${bahmni_user}:${bahmni_user} ${bahmniConfigDirectory}",
-    path     => "${os_path}",
-    require  => Exec["copy_implementation_config"]
+    command => "chown -R ${bahmni_user}:${bahmni_user} ${bahmniConfigDirectory}",
+    path => "${os_path}",
+    require => Exec["copy_implementation_config"]
   }
 
   exec { "set_permissions_of_bahmni_config" :
     provider => "shell",
-    command  => "chmod -R  777 ${bahmniConfigDirectory}",
-    path     => "${os_path}",
-    require  => Exec["copy_implementation_config"]
+    command => "chmod -R  777 ${bahmniConfigDirectory}",
+    path => "${os_path}",
+    require => Exec["copy_implementation_config"]
   }
 }
