984a47fd376958f8d0abf9c463aa40a7c075fa9b
Preethi <preethis@thoughtworks.com>
2015-09-01 23:26:06 +0530

    Preethi | Added script to update latest commit SHA in each repo while branching

---
+++ scripts/branchforrelease/updateSHAs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+brew install jq
+RCol='\x1B[0m'; Red='\x1B[0;31m'; Gre='\x1B[0;32m'; Yel='\x1B[0;33m'; Blu='\x1B[0;34m';
+
+declare -a allrepos=("openmrs-module-bahmniapps" "openerp-atomfeed-service" "OpenElis"
+ "bahmni-core" "bahmni-java-utils" "openerp-modules" "openerp-functional-tests" "openmrs-distro-bahmni"
+ "bahmni-environment" "emr-functional-tests" "default-config" "jss-config" "lokbiradari-config" "possible-config"
+  "banyan-config" "search-config" "ashwini-gudalur-config" "bahmni-reports")
+
+rm repo_revisions.properties
+  for repo in "${allrepos[@]}"
+do
+   echo -e "${Gre}Getting latest commit SHA for $repo ${RCol}"
+   echo  -e "https://api.github.com/repos/Bhamni/$repo/commits/master"
+   printf "$repo=" | tr "-" "_" >> repo_revisions.properties
+   curl "https://api.github.com/repos/Bhamni/$repo/commits/master"| jq '.sha'| tr -d  "\"">> repo_revisions.properties
+done
