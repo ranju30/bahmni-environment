d6af42aa4792329b16feefdc913342b51bec6d2e
Sudhakar <sudhakar82@gmail.com>
2015-09-10 15:58:36 +0530

    Sudhakar | #2821 | Correcting script for creating elis revision file

--- scripts/ci/elis_revision_log.sh
+++ scripts/ci/elis_revision_log.sh
@@ -3,12 +3,9 @@ revision='{
     "github": "https://github.com/Bhamni/OpenElis/commit/_sha_"
 }'
 
-echo "----------"
-echo env
-echo "----------"
-jobName=`env | egrep "GO_JOB_NAME=" | sed "s/GO_JOB_NAME=//g"`
-pipelineCount=`env | egrep "GO_PIPELINE_LABEL=" | sed "s/GO_PIPELINE_LABEL=//g"`
-sha=`env | egrep "GO_REVISION=" | sed "s/GO_REVISION=//g"`
+jobName=`env | egrep "GO_PIPELINE_NAME=" | sed "s/GO_PIPELINE_NAME=//g"`
+pipelineCounter=`env | egrep "GO_PIPELINE_COUNTER=" | sed "s/GO_PIPELINE_COUNTER=//g"`
+sha=`env | egrep "GO_REVISION_OPENELIS_GITHUB=" | sed "s/GO_REVISION_OPENELIS_GITHUB=//g"`
 
-echo $revision | sed "s/_pipelineCount_/$pipelineCount/g" | sed "s/_jobname_/$jobName/g" | sed "s/_sha_/$sha/g"
+echo $revision | sed "s/_pipelineCount_/$pipelineCounter/g" | sed "s/_jobname_/$jobName/g" | sed "s/_sha_/$sha/g"
 
