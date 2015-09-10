b7abced39beab574196cd4b5c37014f637bbea46
Sudhakar <sudhakar82@gmail.com>
2015-09-10 15:37:21 +0530

    Sudhakar | #2821 | Correcting script for creating elis revision file

--- scripts/ci/elis_revision_log.sh
+++ scripts/ci/elis_revision_log.sh
@@ -1,11 +1,14 @@
-revision="{
-    go: 'https://ci-bahmni.thoughtworks.com/go/pipelines/value_stream_map/_jobname_/_pipelineCount_',
-    github: 'https://github.com/Bhamni/OpenElis/commit/_sha_'
-}"
+revision='{
+    "go" : "https://ci-bahmni.thoughtworks.com/go/pipelines/value_stream_map/_jobname_/_pipelineCount_",
+    "github": "https://github.com/Bhamni/OpenElis/commit/_sha_"
+}'
 
-jobName=env | egrep "GO_JOB_NAME=" | sed "s/GO_JOB_NAME=//g"
-pipelineCount=env | egrep "GO_PIPELINE_LABEL=" | sed "s/GO_PIPELINE_LABEL=//g"
-sha=env | egrep "GO_REVISION=" | sed "s/GO_REVISION=//g"
+echo "----------"
+echo env
+echo "----------"
+jobName=`env | egrep "GO_JOB_NAME=" | sed "s/GO_JOB_NAME=//g"`
+pipelineCount=`env | egrep "GO_PIPELINE_LABEL=" | sed "s/GO_PIPELINE_LABEL=//g"`
+sha=`env | egrep "GO_REVISION=" | sed "s/GO_REVISION=//g"`
 
 echo $revision | sed "s/_pipelineCount_/$pipelineCount/g" | sed "s/_jobname_/$jobName/g" | sed "s/_sha_/$sha/g"
 
