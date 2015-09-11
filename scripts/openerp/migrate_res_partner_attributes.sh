05bfa8e7c1513018b4494d36a93c7cc19a9875bd
Preethi <preethis@thoughtworks.com>
2015-09-11 16:50:35 +0530

    Preethi, Vikash | Added script to migrate data from old structure of res_partner_attributes to new structure of res_partner_attributes

--- scripts/openerp/migrate_res_partner_attributes.sh
+++ scripts/openerp/migrate_res_partner_attributes.sh
@@ -3,6 +3,7 @@
 set -e
 
 rm -rf /tmp/res_partner_attributes.sql
+rm -rf /tmp/res_partner_attributes_data.sql
 
 echo "Please enter MySQl password when prompted."
 
@@ -30,8 +31,14 @@ select
 psql -l -Upostgres openerp -f /tmp/res_partner_attributes.sql
 
 
+echo "Please enter MySQl password when prompted."
 
+mysql -uroot -p openmrs -e "
+select 'insert into res_partner_attributes(partner_id, create_uid, create_date, write_date, write_uid) select partner_id, max(create_uid), max(create_date), max(write_date), max(write_uid) from res_partner_attributes_old group by partner_id;'
+union
+select concat('update res_partner_attributes new set \"x_', name, '\" = (select value from res_partner_attributes_old old where name = ''', name,''' and old.partner_id = new.partner_id);')
+INTO OUTFILE '/tmp/res_partner_attributes_data.sql'
+     LINES TERMINATED BY '\n'
+ from person_attribute_type;"
 
-
-
-
+psql -l -Upostgres openerp -f /tmp/res_partner_attributes_data.sql
