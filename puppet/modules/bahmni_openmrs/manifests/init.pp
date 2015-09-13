f74da5d39e030e597d4de1664327621c0efd67e3
Sudhakar <sudhakar82@gmail.com>
2015-09-13 20:25:21 +0530

    Sudhakar | #2821 | Added a puppet module to copy the index.html file so that it can be served by tomcat

--- puppet/modules/bahmni_openmrs/manifests/init.pp
+++ puppet/modules/bahmni_openmrs/manifests/init.pp
@@ -1,6 +1,7 @@
 class bahmni_openmrs {
 	contain bahmni_omods
-	contain bahmni_ui_apps 
+	contain bahmni_ui_apps
+	contain bahmni_landing_page
 	contain bahmni_revisions
 	contain bahmni_client_side_logging
 }
