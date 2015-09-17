d0f5a59634f26f7041f0bb7ebf45165ab51519a6
Sudhakar <sudhakar82@gmail.com>
2015-09-17 21:21:12 +0530

    Sudhakar | #2796 | Fix - Updating the download-build.sh to pick the bahmni_installer_<no>.sh from go

--- scripts/download-build.sh
+++ scripts/download-build.sh
@@ -13,6 +13,12 @@ fi
 BRANCH=master
 ARTIFACTS_PIPELINE_VERSION="Latest"
 CONFIG_PIPELINE_VERSION="Latest"
+BUILD_NUMBER=`wget --timeout=3600 --no-check-certificate --user=$GO_USER --password=$GO_PWD --auth-no-challenge  $BASE_URL/go/files/Bahmni_artifacts_$BRANCH/$ARTIFACTS_PIPELINE_VERSION/CollectArtefactsStage/Latest/defaultJob/installer/ -O - -o /dev/null | grep "^bahmni_installer_" | sed "s/bahmni_installer_//g"`
 
-wget --timeout=3600 --no-check-certificate --user=$GO_USER --password=$GO_PWD --auth-no-challenge  $BASE_URL/go/files/Bahmni_artifacts_$BRANCH/$ARTIFACTS_PIPELINE_VERSION/CollectArtefactsStage/Latest/defaultJob/installer -O /packages/build/bahmni_installer.sh
+echo "-----------------------"
+echo "Downloading bahmni_installer_"$BUILD_NUMBER" to /packages/build folder"
+wget --timeout=3600 --no-check-certificate --user=$GO_USER --password=$GO_PWD --auth-no-challenge  $BASE_URL/go/files/Bahmni_artifacts_$BRANCH/$ARTIFACTS_PIPELINE_VERSION/CollectArtefactsStage/Latest/defaultJob/installer/bahmni_installer_$BUILD_NUMBER -O /packages/build/bahmni_installer.sh
+
+echo "-----------------------"
+echo "Downloading "${FACTER_implementation_name}"_config_installer.sh to /packages/build folder"
 wget --timeout=3600 --no-check-certificate --user=$GO_USER --password=$GO_PWD --auth-no-challenge  $BASE_URL/go/files/Build_${FACTER_implementation_name}_config_$BRANCH/$CONFIG_PIPELINE_VERSION/Package/Latest/package/${FACTER_implementation_name}_config_installer.sh -O /packages/build/${FACTER_implementation_name}_config_installer.sh
