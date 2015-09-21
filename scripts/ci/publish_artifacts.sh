a4fbee18d9d9084c70bde2b3ac28d893bdf0e037
Sudhakar <sudhakar82@gmail.com>
2015-09-21 17:51:25 +0530

    Sudhakar | #2796 | Removing the existing installer files before uploading to dropbox folder

--- scripts/ci/publish_artifacts.sh
+++ scripts/ci/publish_artifacts.sh
@@ -7,6 +7,7 @@ VERSION=$2
 sudo mkdir -p $ARTIFACTS_PATH
 sudo chmod 777 -R $ARTIFACTS_PATH
 
+rm -rf $ARTIFACTS_PATH/release_${VERSION}/
 cp -rf release_${VERSION}/ $ARTIFACTS_PATH
 
 cd $ARTIFACTS_PATH
