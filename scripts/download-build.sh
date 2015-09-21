e6b904ede8bf7db54193be6e2be1e6989a4400fc
Sudhakar <sudhakar82@gmail.com>
2015-09-21 13:17:25 +0530

    Sudhakar | #2796 | Updating the download-build.sh to pick the config_installer_<no>.sh from go

--- scripts/download-build.sh
+++ scripts/download-build.sh
@@ -13,12 +13,13 @@ fi
 BRANCH=Released2
 ARTIFACTS_PIPELINE_VERSION="Latest"
 CONFIG_PIPELINE_VERSION="Latest"
-BUILD_NUMBER=`wget --timeout=3600 --no-check-certificate --user=$GO_USER --password=$GO_PWD --auth-no-challenge  $BASE_URL/go/files/Bahmni_artifacts_$BRANCH/$ARTIFACTS_PIPELINE_VERSION/CollectArtefactsStage/Latest/defaultJob/installer/ -O - -o /dev/null | grep "^bahmni_installer_" | sed "s/bahmni_installer_//g"`
 
+BUILD_NUMBER=`wget --timeout=3600 --no-check-certificate --user=$GO_USER --password=$GO_PWD --auth-no-challenge  $BASE_URL/go/files/Bahmni_artifacts_$BRANCH/$ARTIFACTS_PIPELINE_VERSION/CollectArtefactsStage/Latest/defaultJob/installer/ -O - -o /dev/null | grep "^bahmni_installer_" | sed "s/bahmni_installer_//g"`
 echo "-----------------------"
 echo "Downloading bahmni_installer_"$BUILD_NUMBER" to /packages/build folder"
 wget --timeout=3600 --no-check-certificate --user=$GO_USER --password=$GO_PWD --auth-no-challenge  $BASE_URL/go/files/Bahmni_artifacts_$BRANCH/$ARTIFACTS_PIPELINE_VERSION/CollectArtefactsStage/Latest/defaultJob/installer/bahmni_installer_$BUILD_NUMBER -O /packages/build/bahmni_installer.sh
 
+CONFIG_BUILD_NUMBER=`wget --timeout=3600 --no-check-certificate --user=$GO_USER --password=$GO_PWD --auth-no-challenge  $BASE_URL/go/files/Build_${FACTER_implementation_name}_config_$BRANCH/$CONFIG_PIPELINE_VERSION/Package/Latest/package/installer/ -O - -o /dev/null | grep "^"${FACTER_implementation_name}"_config_installer_" | sed "s/"${FACTER_implementation_name}"_config_installer_//g"`
 echo "-----------------------"
-echo "Downloading "${FACTER_implementation_name}"_config_installer.sh to /packages/build folder"
-wget --timeout=3600 --no-check-certificate --user=$GO_USER --password=$GO_PWD --auth-no-challenge  $BASE_URL/go/files/Build_${FACTER_implementation_name}_config_$BRANCH/$CONFIG_PIPELINE_VERSION/Package/Latest/package/${FACTER_implementation_name}_config_installer.sh -O /packages/build/${FACTER_implementation_name}_config_installer.sh
+echo "Downloading "${FACTER_implementation_name}"_config_installer_"$CONFIG_BUILD_NUMBER" to /packages/build folder"
+wget --timeout=3600 --no-check-certificate --user=$GO_USER --password=$GO_PWD --auth-no-challenge  $BASE_URL/go/files/Build_${FACTER_implementation_name}_config_$BRANCH/$CONFIG_PIPELINE_VERSION/Package/Latest/package/installer/${FACTER_implementation_name}_config_installer_$CONFIG_BUILD_NUMBER -O /packages/build/${FACTER_implementation_name}_config_installer.sh
