16958e031e7257170a440d9bdc5ae2e1e684e7f5
Hemanth Gowda <hemanthfeb25@gmail.com>
2015-10-12 18:57:45 +0530

    Hemanth | Fixing copy omod script

--- puppet/modules/bahmni_omods/templates/install_bahmni_omods.sh
+++ puppet/modules/bahmni_omods/templates/install_bahmni_omods.sh
@@ -3,4 +3,4 @@
 rm -rf "<%= @openmrs_modules_dir %>"/*
 
 echo "Installing core bahmni omods"
-find "<%= build_output_dir %>/<%= openmrs_distro_file_name_prefix %>" -type f -regextype posix-extended -not -regex '.*/(appointmentscheduling.*\.omod|coreapps.*\.omod|htmlformentry.*\.omod|reportingrest.*\.omod|referenceapplication.*\.omod|appui.*\.omod|.*atomfeed.*\.omod|bacteriology.*\.omod)' | grep .omod | xargs -I file cp file "<%= openmrs_modules_dir %>"
+find "<%= build_output_dir %>/<%= openmrs_distro_file_name_prefix %>" -type f -regextype posix-extended -not -regex '.*/(appointmentscheduling.*\.omod|coreapps.*\.omod|htmlformentry.*\.omod|reportingrest.*\.omod|referenceapplication.*\.omod|appui.*\.omod|.*atomfeed.*client.*\.omod|bacteriology.*\.omod)' | grep .omod | xargs -I file cp file "<%= openmrs_modules_dir %>"
