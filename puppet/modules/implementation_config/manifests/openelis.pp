041ae2d9e17bfc3e7a0a1b7ba05d42cd2405de10
shashig <shashig@thoughtworks.com>
2015-11-17 17:40:29 +0530

    Shashi, Chethan | Unzipping openelis to bahmni_temp to ensure liquibase jar is present while running migrations

--- puppet/modules/implementation_config/manifests/openelis.pp
+++ puppet/modules/implementation_config/manifests/openelis.pp
@@ -14,19 +14,20 @@ class implementation_config::openelis {
     path      => "${os_path}",
     onlyif    => "test -f ${build_output_dir}/${implementation_name}_config/openelis/images/labLogo.jpg"
   }
-  
-  file { "${build_output_dir}/OpenElis.zip" : ensure => absent, purge => true}
+
+  file { "${build_output_dir}/OpenElis.zip" : ensure => present}
 
   exec { "bahmni_openelis_codebase_for_liquibase_jar" :
     provider => "shell",
     command  => "unzip -o -q ${build_output_dir}/OpenElis.zip -d ${temp_dir} ${deployment_log_expression}",
     path     => "${os_path}",
-    onlyif    => "test -f ${build_output_dir}/${implementation_name}_config/migrations/openelis/liquibase.xml",
+    onlyif    => "test -f ${build_output_dir}/${implementation_name}_config/openelis/migrations/liquibase.xml",
     require   => [File["${build_output_dir}/OpenElis.zip"]]
   }
 
   implementation_config::migrations { "implementation_config_migrations_openelis":
     implementation_name => "${implementation_name}",
-    app_name            => "openelis"
+    app_name            => "openelis",
+    require => [Exec["bahmni_openelis_codebase_for_liquibase_jar"]]
   }
 }
