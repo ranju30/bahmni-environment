5e9eef6efcedee1f55f29e56a133b91574ea3d27
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-11-17 15:59:58 +0530

    Jaswanth | Added puppet script to create bahmni_reports.properties file in go agent

---
+++ puppet/modules/bahmni_reports/manifests/integration_tests.pp
@@ -0,0 +1,19 @@
+class bahmni_reports::integration_tests {
+  $go_agent_home = "/var/go"
+  $go_user = "go"
+
+  file { "${go_agent_home}/.bahmni-reports" :
+    ensure  => directory,
+    owner   => "${go_user}",
+    group   => "${go_user}",
+    mode    => 554
+  }
+
+  file { "${go_agent_home}/.bahmni-reports/bahmni-reports.properties" :
+    ensure  => present,
+    content => template("bahmni_reports/bahmni-reports.properties.erb"),
+    owner   => "${go_user}",
+    group   => "${go_user}",
+    mode    => 554
+  }
+}
