7a7dc806c23d03eaf3f91e4075d908445e6511a9
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-11-17 23:36:28 +0530

    Jaswanth | Adding script to copy reports properties file to go agent

--- puppet/modules/bahmni_reports/manifests/integration_tests.pp
+++ puppet/modules/bahmni_reports/manifests/integration_tests.pp
@@ -9,6 +9,14 @@ class bahmni_reports::integration_tests {
     mode    => 554
   }
 
+  file { "${go_agent_home}/.bahmni-reports/bahmni-reports.properties" :
+    ensure  => present,
+    content => template("bahmni_reports/bahmni-reports.properties.erb"),
+    owner   => "${go_user}",
+    group   => "${go_user}",
+    mode    => 554
+  }
+
   file { "${go_agent_home}/.bahmni-reports/bahmni-reports-test.properties" :
     ensure  => present,
     content => template("bahmni_reports/bahmni-reports-test.properties.erb"),
