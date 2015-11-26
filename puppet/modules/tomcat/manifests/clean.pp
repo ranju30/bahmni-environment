f3680941e5a48687f3976f18e0b80c7be27a5bd2
shashig <shashig@thoughtworks.com>
2015-11-26 11:03:53 +0530

    Shashi, Hanisha | Removing the contents of temp and work of tomcat

--- puppet/modules/tomcat/manifests/clean.pp
+++ puppet/modules/tomcat/manifests/clean.pp
@@ -1,6 +1,6 @@
 class tomcat::clean {
     exec { "remove_work_and_temp":
-     	command => "rm -rf ${tomcatInstallationDirectory}/work ${tomcatInstallationDirectory}/temp ${deployment_log_expression}",
+     	command => "rm -rf ${tomcatInstallationDirectory}/work/* ${tomcatInstallationDirectory}/temp/* ${deployment_log_expression}",
 	    path => "${os_path}",
 	    user => "${bahmni_user}"
     } 
