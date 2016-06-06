d4ff2802c9bd9f561a67843092c44ee3f1e5982b
Dé <rahul080327@gmail.com>
2016-06-06 17:55:45 +0530

    Rahul, Jaya | #1708 | backing up only openmrs for mysql backup

--- scripts/backup-mysql.sh
+++ scripts/backup-mysql.sh
@@ -6,9 +6,9 @@ if [ -z $rootPassword ]; then
     echo "Please provide a password for mysql root"
 	echo "[USAGE] $0 <mysqlRootPassword> [BACKUP_DIR]"
 	exit 1
-fi	
+fi
 
 TIME=`date +%Y%m%d_%H%M%S`
 BACKUP_DIR=${2:-/backup}
 
-mysqldump -uroot -p$rootPassword --all-databases --routines | gzip > $BACKUP_DIR/mysql_backup_$TIME.sql.gz
+mysqldump -uroot -p$rootPassword openmrs | gzip > $BACKUP_DIR/mysql_backup_$TIME.sql.gz
